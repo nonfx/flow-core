3ff4ed260c3000761a4497feb9e58f97b2447321
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-07-31 13:42:02 +0000

    deploy: 524c428750443247feb429d88442aa09c6734077

---
+++ assets/f-emoji-picker-51c9432a.js
@@ -0,0 +1,75 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{M as t,C as d,b as n,A as l}from"./index-1849ba66.js";import{F as j,P as x,V as o,C as h,a,b as p,S as m,c as u,R as f,I as b,E as g,d as y,e as v,f as k}from"./f-emoji-picker.stories-a103207f.js";import{u as c}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-6895a86d.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./lit-html-b1a28dc3.js";function i(r){const s=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a"},c(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(t,{of:j}),`
+`,e.jsx(s.h1,{id:"f-emoji-picker",children:"f-emoji-picker"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-emoji-picker helps to select emojis as an input."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-emoji-picker--playground",children:"Playground"})}),`
+`,e.jsx(d,{children:e.jsx(n,{of:x})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Variants are various visual representations of an input field."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:o})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"curved"}),e.jsx("td",{children:"border radius of 4px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"round"}),e.jsx("td",{children:"border radius of 22px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"block"}),e.jsx("td",{children:'works well with cateogry="transparent"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"category",children:"category"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Category decides the background of f-emoji-picker input box"}),`
+`,e.jsx(d,{children:e.jsx(n,{of:h})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"fill"}),e.jsx("td",{children:"background is filled"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"outline"}),e.jsx("td",{children:"empty background with border"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"transparent"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"value",children:"value"}),`
+`,e.jsx(s.p,{children:"Defines the value of an f-emoji-picker in terms of emojis."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:a})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"placeholder",children:"placeholder"}),`
+`,e.jsx(s.p,{children:"Defines the placeholder text for f-emoji-picker."}),`
+`,e.jsx("p",{className:"note-section",children:"Note: placeholder must always be an emoji"}),`
+`,e.jsx(d,{children:e.jsx(n,{of:p})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-emoji-picker can have 2 sizes."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:m})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:'height="28px"'}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:'height="36px"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"state",children:"state"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:u})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"default"}),e.jsx("td",{}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"primary"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"success"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"danger"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"warning"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"recent",children:"recent"}),`
+`,e.jsx(s.p,{children:"recently used emojis are added, as array of strings with name of emojis."}),`
+`,e.jsx("code",{children:'Eg: ["office", "compass", "luggage", "hourglass", "gem", "computer"]'}),`
+`,e.jsx(d,{children:e.jsx(n,{of:f})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"include",children:"include"}),`
+`,e.jsx(s.p,{children:"decides which all category should be displayed inside emoji-picker popover"}),`
+`,e.jsx("code",{children:'Eg: ["frequent", "people", "flags"]'}),`
+`,e.jsx(d,{children:e.jsx(n,{of:b})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"exclude",children:"exclude"}),`
+`,e.jsx(s.p,{children:"decides which all category should be excluded from inside emoji-picker popover"}),`
+`,e.jsx("code",{children:'Eg: ["frequent", "people", "flags"]'}),`
+`,e.jsx(d,{children:e.jsx(n,{of:g})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"exclude-emojis",children:"exclude-emojis"}),`
+`,e.jsx(s.p,{children:"decides which all emojis should be excluded from inside emoji-picker popover"}),`
+`,e.jsx("code",{children:'Eg: ["office"]'}),`
+`,e.jsx(d,{children:e.jsx(n,{of:y})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"custom",children:"custom"}),`
+`,e.jsx(s.p,{children:"adds custom emojis"}),`
+`,e.jsx(d,{children:e.jsx(n,{of:v})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"flags",children:"Flags"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(d,{children:e.jsx(n,{of:k})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Name"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"disabled"}),e.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"clear"}),e.jsx("td",{children:e.jsx(s.p,{children:`Displays a close icon-button on the right side of the input that allows the user to clear
+the input value`})}),e.jsx("td",{children:e.jsx("code",{children:"true"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"close-on-select"}),e.jsx("td",{children:"closes popover on emoji-select"}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]})]})}),`
+`,e.jsx(l,{of:"f-emoji-pciker"})]})}function A(r={}){const{wrapper:s}=Object.assign({},c(),r.components);return s?e.jsx(s,Object.assign({},r,{children:e.jsx(i,r)})):i(r)}export{A as default};
