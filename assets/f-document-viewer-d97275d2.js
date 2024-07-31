592865204a5d202bfb7b198c1d4b364750b9f0b1
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-07-31 14:02:37 +0000

    deploy: 08dd6141bf8faf07209bede836a3dd887e384288

---
+++ assets/f-document-viewer-d97275d2.js
@@ -0,0 +1,31 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{M as l,C as s,b as i}from"./index-845678f1.js";import"./lit-html-b1a28dc3.js";import"./unsafe-svg-e29b2cc0.js";import{F as c,P as d,C as j,J as p,a as h,L as a}from"./f-document-viewer.stories-d4b74bcc.js";import{u as t}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-531d73c3.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./directive-12249aa5.js";import"./lit-element-3bb9b17f.js";import"./index-3a83ac04.js";__STORYBOOK_MODULE_CLIENT_API__;function r(o){const e=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a"},t(),o.components);return n.jsxs(n.Fragment,{children:[n.jsx(l,{of:c}),`
+`,n.jsx(e.h1,{id:"f-document-viewer",children:"f-document-viewer"}),`
+`,n.jsx("br",{}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Accordion is a wrapper component which reveals or hide the content on click of header of the component."}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h3,{id:"playground",children:n.jsx(e.a,{href:"/story/ollion-flow-core-f-document-viewer--playground",children:"Playground"})}),`
+`,n.jsx(s,{children:n.jsx(i,{of:d})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"content",children:"content"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Content represents the json schema provided to dispaly contents in viewer."}),`
+`,n.jsx(s,{children:n.jsx(i,{of:j})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h1,{id:"flags",children:"Flags"}),`
+`,n.jsx(e.h2,{id:"jump-links",children:"jump-links"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Flag to toggle the display of jump-links"}),`
+`,n.jsx(s,{children:n.jsx(i,{of:p})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"collapsible-jump-links",children:"collapsible-jump-links"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Flag to toggle the notch present to toggle jump-links"}),`
+`,n.jsx(s,{children:n.jsx(i,{of:h})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"level-selector",children:"level-selector"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Flag to toggle the level-selector"}),`
+`,n.jsx(s,{children:n.jsx(i,{of:a})})]})}function J(o={}){const{wrapper:e}=Object.assign({},t(),o.components);return e?n.jsx(e,Object.assign({},o,{children:n.jsx(r,o)})):r(o)}export{J as default};
