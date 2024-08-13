fe00544c24b4de3eb971089af3017f748f6bc665
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-08-13 15:37:37 +0000

    deploy: 81bd5a20cbda1503233ac01b4b15ab78963da124

---
+++ assets/release-notes-a37be1b6.js
@@ -0,0 +1,17 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import"./blocks-2646952b.js";import{u as r}from"./index-a1cf9e47.js";import{M as s,e as i}from"./index-4190d949.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-883fce6c.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";const a=`<h4 className="margin-btm-8">Release Notes</h4>
+
+# Change Log
+
+## [0.0.2] - 2024-08-07
+
+### Patch
+
+- \`peerDependencies\` updated
+
+## [0.0.1] - 2024-08-02
+
+### Patch
+
+- first release.
+`;function o(t){return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"@nonfx/flow-icons/Release Notes"}),`
+`,n.jsx(i,{children:a})]})}function b(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(o,t)})):o()}export{b as default};
