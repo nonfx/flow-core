f80bb6ff355698b649ea8ac7ae15bc2d3b072d8a
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-08-28 12:09:50 +0000

    deploy: d3aa5801523d3e9a9ee26bae4a208d00dab359fe

---
+++ assets/f-tab-efb87aa4.js
@@ -0,0 +1,39 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{M as l,C as r,b as s,A as a}from"./index-5ce17318.js";import"./lit-html-b1a28dc3.js";import"./unsafe-svg-e29b2cc0.js";import{F as c,P as o,V as h,D as x,N as j,A as b}from"./f-tab.stories-d46b6de8.js";import{u as d}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-d10c8158.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./directive-12249aa5.js";__STORYBOOK_MODULE_CLIENT_API__;function i(t){const e=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a",ul:"ul",li:"li"},d(),t.components);return n.jsxs(n.Fragment,{children:[n.jsx(l,{of:c}),`
+`,n.jsx(e.h1,{id:"f-tab",children:"f-tab"}),`
+`,n.jsx("br",{}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx("hr",{}),`
+`,n.jsx(e.p,{children:"Tabs are broken into two parts. They are connected by a matching content-[id] for example. Tab node (tab-node-1) : Tab content (tab-content-1)"}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h3,{id:"playground",children:n.jsx(e.a,{href:"/story/components-f-tab--playground",children:"Playground"})}),`
+`,n.jsx(r,{children:n.jsx(s,{of:o})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"variant",children:"variant"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"variant of every tab node"}),`
+`,n.jsx(r,{children:n.jsx(s,{of:h})}),`
+`,n.jsx("table",{className:"custom-table",children:n.jsxs("tbody",{children:[n.jsxs("tr",{children:[n.jsx("td",{children:"Value"}),n.jsx("td",{children:"Description"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"fill"}),n.jsx("td",{children:"takes up $surface-secondary color as background"}),n.jsx("td",{children:n.jsx("code",{children:"default"})})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"transparent"}),n.jsx("td",{children:"transparent background"}),n.jsx("td",{})]})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"direction",children:"direction"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Vertical tabs can be enabled using direction property. By default tabs are horizontal."}),`
+`,n.jsx(r,{children:n.jsx(s,{of:x})}),`
+`,n.jsx("table",{className:"custom-table",children:n.jsxs("tbody",{children:[n.jsxs("tr",{children:[n.jsx("td",{children:"Value"}),n.jsx("td",{children:"Description"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"horizontal"}),n.jsx("td",{children:"horizontally aligned tabs"}),n.jsx("td",{children:n.jsx("code",{children:"default"})})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"vertical"}),n.jsx("td",{children:"vertic aligned tabs"}),n.jsx("td",{})]})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"node-width",children:"node-width"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Defines the width of the tab nodes."}),`
+`,n.jsx(r,{children:n.jsx(s,{of:j})}),`
+`,n.jsx("table",{className:"custom-table",children:n.jsxs("tbody",{children:[n.jsxs("tr",{children:[n.jsx("td",{children:"Value"}),n.jsx("td",{children:"Description"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"fill"}),n.jsx("td",{children:"Equal width, filling parent container"}),n.jsx("td",{children:n.jsx("code",{children:"default"})})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"hug-content"}),n.jsx("td",{children:"Variable width"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"num value"}),n.jsx("td",{children:"Fixed width, user defined "}),n.jsx("td",{})]})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"alignment",children:"alignment"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"The alignment property aligns a f-tab bar with respect to the parent container."}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"For horizontal f-tabs, content can align to left, center, or right."}),`
+`,n.jsx(e.li,{children:"For vertical f-tabs, content can align to top, middle, or bottom."}),`
+`]}),`
+`,n.jsx(r,{children:n.jsx(s,{of:b})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(a,{of:"f-tab"})]})}function A(t={}){const{wrapper:e}=Object.assign({},d(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(i,t)})):i(t)}export{A as default};
---
+++ assets/release-notes-416b0d2c.js
@@ -0,0 +1,106 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import"./blocks-2646952b.js";import{u as s}from"./index-a1cf9e47.js";import{M as a,e as d}from"./index-5ce17318.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-d10c8158.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";const r=`<h4 className="margin-btm-8">Release Notes</h4>
+
+# Change Log
+
+## [2.1.6] - 2024-08-20
+
+### Patch Changes
+
+- nested dependency issue
+
+## [2.1.5] - 2024-08-09
+
+### Patch Changes
+
+- nested dependency issue
+
+## [2.1.4] - 2024-08-09
+
+### Patch Changes
+
+- nested dependency issue
+
+## [2.1.3] - 2024-08-09
+
+### Patch Changes
+
+- \`f-md-editor\` peerDependencies updated.
+
+## [2.1.2] - 2024-07-08
+
+### Patch Changes
+
+- \`f-md-editor\` stories added.
+- minor css changes when used inside \`f-div\`.
+
+## [2.1.1] - 2024-07-08
+
+### Patch Changes
+
+- \`f-md-editor\` type fixed.
+- \`vue\` component types updated.
+
+## [2.1.0] - 2023-11-27
+
+### Minor Changes
+
+- \`lit\` upgraded to v3.
+
+## [2.0.3] - 2023-10-12
+
+### Patch Changes
+
+- Remove sideEffects because the components require registration via import
+- Updated dependencies
+- \`@nonfx/flow-core-config@1.1.3\`
+- \`@nonfx/flow-core@2.0.3\`
+
+## [2.0.2] - 2023-10-10
+
+### Patch Changes
+
+- a2de106: Fix platform types
+- Updated dependencies [a2de106]
+- \`@nonfx/flow-core-config@1.1.1\`
+- \`@nonfx/flow-core@2.0.1\`
+
+## [2.0.1-beta.0] - 2023-10-10
+
+### Patch Changes
+
+- Fix platform types
+- Updated dependencies
+- \`@nonfx/flow-core-config@1.1.1-beta.0\`
+- \`@nonfx/flow-core@2.0.1-beta.0\`
+
+## [2.0.0] - 2023-10-10
+
+### Major Changes
+
+- Migrated to monorepo structure and removed the need for custom CSS
+
+### Patch Changes
+
+- Updated dependencies
+- \`@nonfx/flow-core@2.0.0\`
+<hr className="margin-btm-32" />
+
+## [1.0.0] - 2023-07-20
+
+### Feature
+
+- BREAKING : Using lightDom for markdown customization (This change will apply gloabl or userAgent styles in md viewer)
+
+## [0.0.2] - 2023-07-18
+
+### Improvements
+
+- \`f-md-editor\` : edit mode css updated
+
+## [0.0.1] - 2023-07-12
+
+### Features
+
+- \`f-md-editor\` : component added
+`;function o(e){return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"@nonfx/flow-md-editor/Release Notes"}),`
+`,n.jsx(d,{children:r})]})}function M(e={}){const{wrapper:t}=Object.assign({},s(),e.components);return t?n.jsx(t,Object.assign({},e,{children:n.jsx(o,e)})):o()}export{M as default};
