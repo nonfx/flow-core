681a146f65052bd4171ff4fd573efddca40315b2
Vikas <vikas.awaghade@hotmail.com>
2024-09-05 09:09:08 +0530

    NFX-298 icon minification updated

--- packages/flow-icons/src/flow-policy-icon/icon-pack.ts
+++ packages/flow-icons/src/flow-policy-icon/icon-pack.ts
@@ -1,17 +1,17 @@
-import i_policy_nist from './svg/i-policy-nist';
-import i_policy_hipaa from './svg/i-policy-hipaa';
-import i_policy_cis from './svg/i-policy-cis';
-import i_policy_pci from './svg/i-policy-pci';
-import i_policy_ISO27001 from './svg/i-policy-ISO27001';
-import i_policy_gdpr from './svg/i-policy-gdpr';
-import i_policy_soc2 from './svg/i-policy-soc2';
+import i_policy_nist from "./svg/i-policy-nist";
+import i_policy_hipaa from "./svg/i-policy-hipaa";
+import i_policy_cis from "./svg/i-policy-cis";
+import i_policy_pci from "./svg/i-policy-pci";
+import i_policy_ISO27001 from "./svg/i-policy-ISO27001";
+import i_policy_gdpr from "./svg/i-policy-gdpr";
+import i_policy_soc2 from "./svg/i-policy-soc2";
 const IconPack = {
-    'i-policy-nist': i_policy_nist,
-    'i-policy-hipaa': i_policy_hipaa,
-    'i-policy-cis': i_policy_cis,
-    'i-policy-pci': i_policy_pci,
-    'i-policy-ISO27001': i_policy_ISO27001,
-    'i-policy-gdpr': i_policy_gdpr,
-    'i-policy-soc2': i_policy_soc2,
+	"i-policy-nist": i_policy_nist,
+	"i-policy-hipaa": i_policy_hipaa,
+	"i-policy-cis": i_policy_cis,
+	"i-policy-pci": i_policy_pci,
+	"i-policy-ISO27001": i_policy_ISO27001,
+	"i-policy-gdpr": i_policy_gdpr,
+	"i-policy-soc2": i_policy_soc2
 } as Record<string, string>;
 export default IconPack;
