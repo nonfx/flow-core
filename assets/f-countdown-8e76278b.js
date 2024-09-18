4c53dbf62680e49813db7d60c82510d0b16d9a94
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-09-18 09:54:03 +0000

    deploy: 29f4bde74619ba12594f7f19bafc86770a1a310f

---
+++ assets/f-countdown-8e76278b.js
@@ -0,0 +1,44 @@
+import{j as s}from"./jsx-runtime-ffb262ed.js";import{M as l,C as n,b as e}from"./index-b7a2e2aa.js";import{F as c,P as x,D as j,C as o,S as a,a as h,L as p}from"./f-countdown.stories-95e3eba3.js";import{u as i}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-f447ba06.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./lit-html-b1a28dc3.js";function t(d){const r=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a"},i(),d.components);return s.jsxs(s.Fragment,{children:[s.jsx(l,{of:c}),`
+`,s.jsx(r.h1,{id:"f-countdown",children:"f-countdown"}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h2,{id:"overview",children:"Overview"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(r.p,{children:"Breadcrumbs are secondary navigations which shows hierarchy for a user’s location within an app/product."}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h3,{id:"playground",children:s.jsx(r.a,{href:"/story/components-f-breadcrumb--playground",children:"Playground"})}),`
+`,s.jsx(n,{children:s.jsx(e,{of:x})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h2,{id:"duration",children:"duration"}),`
+`,s.jsx(r.p,{children:"Duration for f-countdown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:j})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h2,{id:"category",children:"category"}),`
+`,s.jsx(r.p,{children:"Category for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:o})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"fill"}),s.jsx("td",{children:"background is filled with respective state"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h2,{id:"size",children:"size"}),`
+`,s.jsx(r.p,{children:"Sizes for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:a})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"large"}),s.jsx("td",{children:"44px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"medium"}),s.jsx("td",{children:"36px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"small"}),s.jsx("td",{children:"28px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"x-small"}),s.jsx("td",{children:"20px"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h2,{id:"state",children:"state"}),`
+`,s.jsx(r.p,{children:"States for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:h})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"default"}),s.jsx("td",{children:"--color-primary-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"primary"}),s.jsx("td",{children:"--color-primary-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"success"}),s.jsx("td",{children:"--color-success-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"warning"}),s.jsx("td",{children:"--color-warning-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"danger"}),s.jsx("td",{children:"--color-danger-default"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h2,{id:"label-placement",children:"label-placement"}),`
+`,s.jsx(r.p,{children:"label-placement for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:p})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"top"}),s.jsx("td",{children:"label placed at top"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"left"}),s.jsx("td",{children:"label placed at left"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"bottom"}),s.jsx("td",{children:"label placed at bottom"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"right"}),s.jsx("td",{children:"label placed at right"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"none"}),s.jsx("td",{children:"label is hidden"}),s.jsx("td",{})]})]})})]})}function V(d={}){const{wrapper:r}=Object.assign({},i(),d.components);return r?s.jsx(r,Object.assign({},d,{children:s.jsx(t,d)})):t(d)}export{V as default};
---
+++ assets/release-notes-286854a3.js
@@ -0,0 +1,60 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import"./blocks-2646952b.js";import{u as s}from"./index-a1cf9e47.js";import{M as r,e as a}from"./index-b7a2e2aa.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-f447ba06.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";const i=`<h4 className="margin-btm-8">Release Notes</h4>
+
+# Change Log
+
+## [1.0.2] - 2024-09-17
+
+### Patch
+
+- Icons updated
+
+## [1.0.1] - 2024-09-05
+
+### Patch
+
+- Policy icons import fixed.
+
+## [1.0.0] - 2024-09-04
+
+### Major
+
+- \`sass-system\` and \`engineering\` icons removed
+- bundle size reduced to 1.1MB from 3MB
+
+## [0.0.6] - 2024-08-29
+
+### Patch
+
+- icons updated
+
+## [0.0.5] - 2024-08-29
+
+### Patch
+
+- icons updated
+
+## [0.0.4] - 2024-08-28
+
+### Patch
+
+- icons updated
+
+## [0.0.3] - 2024-08-20
+
+### Patch
+
+- \`peerDependencies\` updated
+
+## [0.0.2] - 2024-08-07
+
+### Patch
+
+- \`peerDependencies\` updated
+
+## [0.0.1] - 2024-08-02
+
+### Patch
+
+- first release.
+`;function o(e){return n.jsxs(n.Fragment,{children:[n.jsx(r,{title:"@nonfx/flow-icons/Release Notes"}),`
+`,n.jsx(a,{children:i})]})}function D(e={}){const{wrapper:t}=Object.assign({},s(),e.components);return t?n.jsx(t,Object.assign({},e,{children:n.jsx(o,e)})):o()}export{D as default};
