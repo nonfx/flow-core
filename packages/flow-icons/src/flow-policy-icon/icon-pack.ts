bbbcab13cfd1d47a3a94b928b7517d72225c2744
Vikas Awaghade <67629551+vikas-cldcvr@users.noreply.github.com>
2024-10-01 12:04:45 +0530

    NFX-447 Icons updated (#33)
    * NFX-447 icons updated
    
    * NFX-447 icons updated

--- packages/flow-icons/src/flow-policy-icon/icon-pack.ts
+++ packages/flow-icons/src/flow-policy-icon/icon-pack.ts
@@ -1,19 +1,29 @@
-import i_policy_ISO27001 from "./svg/i-policy-ISO27001";
 import i_policy_cis from "./svg/i-policy-cis";
-import i_policy_pci from "./svg/i-policy-pci";
-import i_policy_nist from "./svg/i-policy-nist";
+import i_policy_fca from "./svg/i-policy-fca";
+import i_policy_ISO27001 from "./svg/i-policy-ISO27001";
 import i_policy_mastrm from "./svg/i-policy-mastrm";
-import i_policy_hipaa from "./svg/i-policy-hipaa";
+import i_policy_pci from "./svg/i-policy-pci";
+import i_policy_ojk from "./svg/i-policy-ojk";
+import i_policy_nist from "./svg/i-policy-nist";
 import i_policy_gdpr from "./svg/i-policy-gdpr";
+import i_policy_hipaa from "./svg/i-policy-hipaa";
 import i_policy_soc2 from "./svg/i-policy-soc2";
+import i_policy_bank_of_malaysia from "./svg/i-policy-bank-of-malaysia";
+import i_policy_sama from "./svg/i-policy-sama";
+import i_policy_arpa from "./svg/i-policy-arpa";
 const IconPack = {
-	"i-policy-ISO27001": i_policy_ISO27001,
 	"i-policy-cis": i_policy_cis,
-	"i-policy-pci": i_policy_pci,
-	"i-policy-nist": i_policy_nist,
+	"i-policy-fca": i_policy_fca,
+	"i-policy-ISO27001": i_policy_ISO27001,
 	"i-policy-mastrm": i_policy_mastrm,
-	"i-policy-hipaa": i_policy_hipaa,
+	"i-policy-pci": i_policy_pci,
+	"i-policy-ojk": i_policy_ojk,
+	"i-policy-nist": i_policy_nist,
 	"i-policy-gdpr": i_policy_gdpr,
-	"i-policy-soc2": i_policy_soc2
+	"i-policy-hipaa": i_policy_hipaa,
+	"i-policy-soc2": i_policy_soc2,
+	"i-policy-bank-of-malaysia": i_policy_bank_of_malaysia,
+	"i-policy-sama": i_policy_sama,
+	"i-policy-arpa": i_policy_arpa
 } as Record<string, string>;
 export default IconPack;
--- packages/flow-icons/src/flow-product-icon/icon-pack.ts
+++ packages/flow-icons/src/flow-product-icon/icon-pack.ts
@@ -1,135 +1,133 @@
-import p_google from "./svg/p-google";
 import p_docker from "./svg/p-docker";
-import p_aws_light from "./svg/p-aws-light";
+import p_google from "./svg/p-google";
 import p_azure_1 from "./svg/p-azure-1";
-import p_kubernetes from "./svg/p-kubernetes";
 import p_gcp from "./svg/p-gcp";
-import p_aws_dark from "./svg/p-aws-dark";
-import p_azure from "./svg/p-azure";
+import p_kubernetes from "./svg/p-kubernetes";
 import p_gitlab from "./svg/p-gitlab";
+import p_azure from "./svg/p-azure";
+import p_aws_dark from "./svg/p-aws-dark";
+import p_aws_light from "./svg/p-aws-light";
 import p_sql_server from "./svg/p-sql-server";
 import p_discord from "./svg/p-discord";
-import p_jira from "./svg/p-jira";
-import p_slack from "./svg/p-slack";
-import p_prometheus from "./svg/p-prometheus";
-import l_mongodb from "./svg/l-mongodb";
-import p_mysql from "./svg/p-mysql";
-import p_postgresql from "./svg/p-postgresql";
 import p_asana from "./svg/p-asana";
+import p_jira from "./svg/p-jira";
+import p_postgresql from "./svg/p-postgresql";
+import p_prometheus from "./svg/p-prometheus";
+import p_slack from "./svg/p-slack";
+import p_mysql from "./svg/p-mysql";
 import p_mongodb from "./svg/p-mongodb";
 import p_oracle from "./svg/p-oracle";
 import p_memcache from "./svg/p-memcache";
-import p_datapipes from "./svg/p-datapipes";
-import p_tableau from "./svg/p-tableau";
-import p_tomcat from "./svg/p-tomcat";
 import p_redis from "./svg/p-redis";
+import p_tomcat from "./svg/p-tomcat";
+import p_tableau from "./svg/p-tableau";
+import p_golang from "./svg/p-golang";
 import p_dataiku from "./svg/p-dataiku";
 import p_jupyter from "./svg/p-jupyter";
-import p_golang from "./svg/p-golang";
+import p_datapipes from "./svg/p-datapipes";
 import p_hadoop from "./svg/p-hadoop";
 import p_postmark from "./svg/p-postmark";
-import p_apache from "./svg/p-apache";
-import p_vscode from "./svg/p-vscode";
-import p_ngnix from "./svg/p-ngnix";
-import p_lightspeed from "./svg/p-lightspeed";
-import p_django from "./svg/p-django";
-import p_nodejs from "./svg/p-nodejs";
 import p_ruby_on_rails from "./svg/p-ruby-on-rails";
-import p_elasticsearch from "./svg/p-elasticsearch";
 import p_apache_spark from "./svg/p-apache-spark";
+import p_django from "./svg/p-django";
+import p_apache from "./svg/p-apache";
+import p_elasticsearch from "./svg/p-elasticsearch";
+import p_ngnix from "./svg/p-ngnix";
+import p_vscode from "./svg/p-vscode";
 import p_jenkins from "./svg/p-jenkins";
+import p_nodejs from "./svg/p-nodejs";
+import p_lightspeed from "./svg/p-lightspeed";
+import p_terraform from "./svg/p-terraform";
+import p_snowflake from "./svg/p-snowflake";
+import p_github from "./svg/p-github";
+import p_fortify from "./svg/p-fortify";
+import p_wordpress from "./svg/p-wordpress";
+import p_sonarcloud from "./svg/p-sonarcloud";
+import p_sonatype from "./svg/p-sonatype";
+import p_sonarqube_dark from "./svg/p-sonarqube-dark";
 import p_sonarqube_light from "./svg/p-sonarqube-light";
 import p_grafana from "./svg/p-grafana";
-import p_sonarqube_dark from "./svg/p-sonarqube-dark";
-import p_terraform from "./svg/p-terraform";
-import p_sonatype from "./svg/p-sonatype";
-import p_wordpress from "./svg/p-wordpress";
-import p_snowflake from "./svg/p-snowflake";
-import p_fortify from "./svg/p-fortify";
-import p_github from "./svg/p-github";
-import p_sonarcloud from "./svg/p-sonarcloud";
-import p_opa from "./svg/p-opa";
+import p_starchitect_mark_light from "./svg/p-starchitect-mark-light";
+import p_nonfx_full_light from "./svg/p-nonfx-full-light";
 import p_nonfx_full_dark from "./svg/p-nonfx-full-dark";
 import p_nonfx_mark from "./svg/p-nonfx-mark";
-import p_starchitect_mark_light from "./svg/p-starchitect-mark-light";
-import p_salesforce from "./svg/p-salesforce";
+import p_opa from "./svg/p-opa";
 import p_trino from "./svg/p-trino";
-import p_kafka_dark from "./svg/p-kafka-dark";
-import p_nonfx_full_light from "./svg/p-nonfx-full-light";
 import p_ollion from "./svg/p-ollion";
 import p_kafka_light from "./svg/p-kafka-light";
-import p_starchitect_small_dark from "./svg/p-starchitect-small-dark";
-import p_starchitect_full_light from "./svg/p-starchitect-full-light";
+import p_kafka_dark from "./svg/p-kafka-dark";
+import p_salesforce from "./svg/p-salesforce";
 import p_starchitect_full_light_app from "./svg/p-starchitect-full-light-app";
-import p_starchitect_mark_dark from "./svg/p-starchitect-mark-dark";
 import p_starchitect_full_dark_app from "./svg/p-starchitect-full-dark-app";
+import p_starchitect_small_dark from "./svg/p-starchitect-small-dark";
+import p_starchitect_mark_dark from "./svg/p-starchitect-mark-dark";
 import p_starchitect_full_dark from "./svg/p-starchitect-full-dark";
+import p_starchitect_full_light from "./svg/p-starchitect-full-light";
 const IconPack = {
-	"p-google": p_google,
 	"p-docker": p_docker,
-	"p-aws-light": p_aws_light,
+	"p-google": p_google,
 	"p-azure-1": p_azure_1,
-	"p-kubernetes": p_kubernetes,
 	"p-gcp": p_gcp,
-	"p-aws-dark": p_aws_dark,
-	"p-azure": p_azure,
+	"p-kubernetes": p_kubernetes,
 	"p-gitlab": p_gitlab,
+	"p-azure": p_azure,
+	"p-aws-dark": p_aws_dark,
+	"p-aws-light": p_aws_light,
 	"p-sql-server": p_sql_server,
 	"p-discord": p_discord,
-	"p-jira": p_jira,
-	"p-slack": p_slack,
-	"p-prometheus": p_prometheus,
-	"l-mongodb": l_mongodb,
-	"p-mysql": p_mysql,
-	"p-postgresql": p_postgresql,
 	"p-asana": p_asana,
+	"p-jira": p_jira,
+	"p-postgresql": p_postgresql,
+	"p-prometheus": p_prometheus,
+	"p-slack": p_slack,
+	"p-mysql": p_mysql,
 	"p-mongodb": p_mongodb,
 	"p-oracle": p_oracle,
 	"p-memcache": p_memcache,
-	"p-datapipes": p_datapipes,
-	"p-tableau": p_tableau,
-	"p-tomcat": p_tomcat,
 	"p-redis": p_redis,
+	"p-tomcat": p_tomcat,
+	"p-tableau": p_tableau,
+	"p-golang": p_golang,
 	"p-dataiku": p_dataiku,
 	"p-jupyter": p_jupyter,
-	"p-golang": p_golang,
+	"p-datapipes": p_datapipes,
 	"p-hadoop": p_hadoop,
 	"p-postmark": p_postmark,
-	"p-apache": p_apache,
-	"p-vscode": p_vscode,
-	"p-ngnix": p_ngnix,
-	"p-lightspeed": p_lightspeed,
-	"p-django": p_django,
-	"p-nodejs": p_nodejs,
 	"p-ruby-on-rails": p_ruby_on_rails,
-	"p-elasticsearch": p_elasticsearch,
 	"p-apache-spark": p_apache_spark,
+	"p-django": p_django,
+	"p-apache": p_apache,
+	"p-elasticsearch": p_elasticsearch,
+	"p-ngnix": p_ngnix,
+	"p-vscode": p_vscode,
 	"p-jenkins": p_jenkins,
+	"p-nodejs": p_nodejs,
+	"p-lightspeed": p_lightspeed,
+	"p-terraform": p_terraform,
+	"p-snowflake": p_snowflake,
+	"p-github": p_github,
+	"p-fortify": p_fortify,
+	"p-wordpress": p_wordpress,
+	"p-sonarcloud": p_sonarcloud,
+	"p-sonatype": p_sonatype,
+	"p-sonarqube-dark": p_sonarqube_dark,
 	"p-sonarqube-light": p_sonarqube_light,
 	"p-grafana": p_grafana,
-	"p-sonarqube-dark": p_sonarqube_dark,
-	"p-terraform": p_terraform,
-	"p-sonatype": p_sonatype,
-	"p-wordpress": p_wordpress,
-	"p-snowflake": p_snowflake,
-	"p-fortify": p_fortify,
-	"p-github": p_github,
-	"p-sonarcloud": p_sonarcloud,
-	"p-opa": p_opa,
+	"p-starchitect-mark-light": p_starchitect_mark_light,
+	"p-nonfx-full-light": p_nonfx_full_light,
 	"p-nonfx-full-dark": p_nonfx_full_dark,
 	"p-nonfx-mark": p_nonfx_mark,
-	"p-starchitect-mark-light": p_starchitect_mark_light,
-	"p-salesforce": p_salesforce,
+	"p-opa": p_opa,
 	"p-trino": p_trino,
-	"p-kafka-dark": p_kafka_dark,
-	"p-nonfx-full-light": p_nonfx_full_light,
 	"p-ollion": p_ollion,
 	"p-kafka-light": p_kafka_light,
-	"p-starchitect-small-dark": p_starchitect_small_dark,
-	"p-starchitect-full-light": p_starchitect_full_light,
+	"p-kafka-dark": p_kafka_dark,
+	"p-salesforce": p_salesforce,
 	"p-starchitect-full-light-app": p_starchitect_full_light_app,
-	"p-starchitect-mark-dark": p_starchitect_mark_dark,
 	"p-starchitect-full-dark-app": p_starchitect_full_dark_app,
-	"p-starchitect-full-dark": p_starchitect_full_dark
+	"p-starchitect-small-dark": p_starchitect_small_dark,
+	"p-starchitect-mark-dark": p_starchitect_mark_dark,
+	"p-starchitect-full-dark": p_starchitect_full_dark,
+	"p-starchitect-full-light": p_starchitect_full_light
 } as Record<string, string>;
 export default IconPack;
