4ed44cf1ce0185a0622a986f1eea6e90e0024b35
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-10-29 05:08:44 +0000

    deploy: e6e0eb086466cec432db4677b50cc09ec7062d47

---
+++ assets/About-e1401191.js
@@ -0,0 +1,122 @@
+import{j as t}from"./jsx-runtime-ffb262ed.js";import"./blocks-2646952b.js";import{u as r}from"./index-a1cf9e47.js";import{M as s}from"./index-81792101.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-5de7b6c6.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";const i=`# Flow Code Editor
+
+The Flow code editor is built on the Flow design framework ([website](https://flow.ollion.com/) / [github](https://github.com/ollionorg/flow-core)) using [Monaco Editor](https://microsoft.github.io/monaco-editor/)
+
+# Installation
+
+### 1️⃣ Install flow code editor dependency
+
+\`\`\`
+npm i --save @nonfx/flow-code-editor
+\`\`\`
+
+**Note:** after installation, re-start your application.
+
+<br>
+
+### 2️⃣ Import flow-code-editor into your project
+
+Paste the below snippet in your project and add your application startup/runtime code to it.
+
+\`\`\`javascript
+import "@nonfx/flow-core";
+import "@nonfx/flow-code-editor";
+\`\`\`
+
+<br>
+
+### 3️⃣ For a typescript enabled project (optional)
+
+**Note:** After adding, re-start your application. Make sure you are using version >4.5
+
+**For Vue 3:**
+Copy paste below import types in your \`main.ts\` file.
+
+\`\`\`Javascript
+import "@nonfx/flow-code-editor/dist/types/vue3";
+\`\`\`
+
+<details>
+<summary>For Vue 2</summary>
+
+Copy paste below import types in your \`main.ts\` file.
+
+\`\`\`Javascript
+import "@nonfx/flow-code-editor/dist/types/vue2";
+\`\`\`
+
+</details>
+
+<details>
+<summary>For React</summary>
+
+**React**: Include react type in \`tsconfig.json\` file like below.
+
+\`\`\`json
+"include": ["src", "./node_modules/@nonfx/flow-code-editor/dist/types/react.ts"]
+\`\`\`
+
+</details>
+<br>
+
+# Integration in Vite App
+
+### Add following code snippet in \`vite.config.ts\`
+
+\`\`\`typescript
+import * as fs from "fs";
+import * as path from "path";
+
+const copyDir = (src, dest, callback?: (er: Error) => void) => {
+	const copy = (copySrc, copyDest) => {
+		fs.readdir(copySrc, (err, list) => {
+			if (err) {
+				callback(err);
+				return;
+			}
+			list.forEach(item => {
+				const ss = path.resolve(copySrc, item);
+				fs.stat(ss, (err, stat) => {
+					if (err) {
+						callback(err);
+					} else {
+						const curSrc = path.resolve(copySrc, item);
+						const curDest = path.resolve(copyDest, item);
+
+						if (stat.isFile()) {
+							fs.createReadStream(curSrc).pipe(fs.createWriteStream(curDest));
+						} else if (stat.isDirectory()) {
+							fs.mkdirSync(curDest, { recursive: true });
+							copy(curSrc, curDest);
+						}
+					}
+				});
+			});
+		});
+	};
+
+	fs.access(dest, err => {
+		if (err) {
+			fs.mkdirSync(dest, { recursive: true });
+		}
+		copy(src, dest);
+	});
+};
+
+copyDir("node_modules/@nonfx/flow-code-editor/dist/assets", "assets");
+\`\`\`
+
+### add specify \`assets\` folder in \`assetsInclude\` property.
+
+For example
+
+\`\`\`typescript
+export default defineConfig({
+	plugins: [vue()],
+	base: "",
+	// \`assets\` folder specified here 👇
+	assetsInclude: ["assets"]
+});
+\`\`\`
+`;function o(n){return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"@nonfx/flow-code-editor/About"}),`
+`,t.jsx("f-md-editor",{value:i})]})}function v(n={}){const{wrapper:e}=Object.assign({},r(),n.components);return e?t.jsx(e,Object.assign({},n,{children:t.jsx(o,n)})):o()}export{v as default};
---
+++ assets/f-tab-node-b5babd02.js
@@ -0,0 +1,25 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{M as d,C as t,b as r,A as a}from"./index-81792101.js";import{F as c,P as j,C as x,A as h,D as l}from"./f-tab-node.stories-f0a41da7.js";import{u as i}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-5de7b6c6.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./lit-html-b1a28dc3.js";import"./ref-a55570ea.js";import"./directive-helpers-7e9c1cf5.js";import"./directive-12249aa5.js";function s(o){const e=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a",code:"code"},i(),o.components);return n.jsxs(n.Fragment,{children:[n.jsx(d,{of:c}),`
+`,n.jsx(e.h1,{id:"f-tab-node",children:"f-tab-node"}),`
+`,n.jsx("br",{}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx("hr",{}),`
+`,n.jsx(e.p,{children:"f-tab-node holds the content of every tab."}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h3,{id:"playground",children:n.jsx(e.a,{href:"/story/components-f-tab-node--playground",children:"Playground"})}),`
+`,n.jsx(t,{children:n.jsx(r,{of:j})}),`
+`,n.jsx(e.h2,{id:"content-id",children:"content-id"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsxs(e.p,{children:[n.jsx(e.code,{children:"id"})," to match up with ",n.jsx(e.code,{children:"f-tab-node"})," ",n.jsx(e.code,{children:"content-id"}),"."]}),`
+`,n.jsx(t,{children:n.jsx(r,{of:x})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"active",children:"active"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Blue bottom border appears for the active node. Takes up boolean value."}),`
+`,n.jsx(t,{children:n.jsx(r,{of:h})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"disabled",children:"disabled"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"To disable f-tab-node"}),`
+`,n.jsx(t,{children:n.jsx(r,{of:l})}),`
+`,n.jsx(a,{of:"f-tab-node"})]})}function S(o={}){const{wrapper:e}=Object.assign({},i(),o.components);return e?n.jsx(e,Object.assign({},o,{children:n.jsx(s,o)})):s(o)}export{S as default};
