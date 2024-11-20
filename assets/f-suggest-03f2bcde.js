cc2410a5f98ec9b77c2e1bb270d03bca5251867e
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-11-20 07:12:28 +0000

    deploy: 79c63ecaa3cf4afcc77845da5028280eeb2ab059

---
+++ assets/f-suggest-03f2bcde.js
@@ -0,0 +1,82 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{M as l,C as t,b as n,A as c}from"./index-97153d7a.js";import{F as x,P as h,A as j,V as o,C as a,a as f,b as p,S as u,c as g,d as b,I as m,e as y,f as v,g as N,M as w,h as D}from"./f-suggest.stories-d10423d0.js";import{u as d}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-7ed04fc7.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./lit-html-b1a28dc3.js";import"./i-finput-anatomy-08dacfc4.js";import"./unsafe-svg-e29b2cc0.js";import"./directive-12249aa5.js";function i(r){const s=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a"},d(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(l,{of:x}),`
+`,e.jsx(s.h1,{id:"f-suggest",children:"f-suggest"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-suggest is an input element."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-suggest--playground",children:"Playground"})}),`
+`,e.jsx(t,{children:e.jsx(n,{of:h})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"anatomy",children:"Anatomy"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n,{of:j}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Variants are various visual representations of this field."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:o})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"curved"}),e.jsx("td",{children:"border radius of 4px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"round"}),e.jsx("td",{children:"border radius of 22px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"block"}),e.jsx("td",{children:'works well with cateogry="transparent"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"category",children:"category"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Category decides the background of f-suggest"}),`
+`,e.jsx(t,{children:e.jsx(n,{of:a})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"fill"}),e.jsx("td",{children:"background is filled"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"outline"}),e.jsx("td",{children:"empty background with border"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"transparent"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"value",children:"value"}),`
+`,e.jsx(s.p,{children:"Defines the value of an f-suggest. Validation rules are applied on the value depending on the type property of the f-text-input."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:f})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"placeholder",children:"placeholder"}),`
+`,e.jsx(s.p,{children:"Defines the placeholder text for f-text-input"}),`
+`,e.jsx(t,{children:e.jsx(n,{of:p})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-suggest can have 2 sizes."}),`
+`,e.jsx("p",{className:"note-section",children:"Note: By default size is inherited by the parent f-form."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:u})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:'height="28px"'}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:'eheight="36px"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"state",children:"state"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:g})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"default"}),e.jsx("td",{}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"primary"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"success"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"danger"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"warning"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"suggestions",children:"suggestions"}),`
+`,e.jsx(s.p,{children:"Provides the search suggestions in the form of an array of strings."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:b})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"object of categories"}),e.jsx("td",{children:'for eg. category1:["option1", "option2"]'}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"array os string values"}),e.jsx("td",{children:'for e.g. ["option1", "option2"]'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"icon-left",children:"icon-left"}),`
+`,e.jsx(s.p,{children:"Icon-left enables an icon on the left of the input value."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(s.p,{children:"Note: If f-suggest has a prefix also, then icon-left is placed on the left of the prefix."})}),`
+`,e.jsx(t,{children:e.jsx(n,{of:m})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"icon-right",children:"icon-right"}),`
+`,e.jsx(s.p,{children:"Icon-right enables an icon on the right of the input value."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(s.p,{children:"Note: If f-suggest has a suffix also, then icon-right is placed on the right of the suffix."})}),`
+`,e.jsx(t,{children:e.jsx(n,{of:y})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"prefix",children:"prefix"}),`
+`,e.jsx(s.p,{children:"Prefix property enables a string before the input value."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:v})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"suffix",children:"suffix"}),`
+`,e.jsx(s.p,{children:"Suffix property enables a string on the right side of the input box."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:N})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"max-length",children:"max-length"}),`
+`,e.jsx(s.p,{children:"This shows the character count while typing and auto limits after reaching the max length."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:w})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"flags",children:"Flags"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t,{children:e.jsx(n,{of:D})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Name"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"loading"}),e.jsx("td",{children:"Loader appears on right ride of input box."}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"disabled"}),e.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"clear"}),e.jsx("td",{children:e.jsx(s.p,{children:`Displays a close icon-button on the right side of the input that allows the user to clear
+the input value`})}),e.jsx("td",{children:e.jsx("code",{children:"true"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"read-only"}),e.jsx("td",{children:"Only Text could be read, can't be edited"}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]})]})}),`
+`,e.jsx(c,{of:"f-suggest"})]})}function q(r={}){const{wrapper:s}=Object.assign({},d(),r.components);return s?e.jsx(s,Object.assign({},r,{children:e.jsx(i,r)})):i(r)}export{q as default};
