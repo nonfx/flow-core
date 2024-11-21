0bf563a3a688eefb9b0e330eb26b7c1d9cc39818
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-11-21 12:35:10 +0000

    deploy: 903f5964ecbcbdbadab0e60a2450d02f59f86490

---
+++ assets/f-switch-03ff17f7.js
@@ -0,0 +1,38 @@
+import{j as s}from"./jsx-runtime-ffb262ed.js";import{M as c,C as t,b as d,A as l}from"./index-b213078e.js";import{F as x,P as j,A as a,V as o,S as h,a as m,b as u}from"./f-switch.stories-95114409.js";import{u as i}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-c841b035.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./lit-html-b1a28dc3.js";import"./unsafe-svg-e29b2cc0.js";import"./directive-12249aa5.js";function r(n){const e=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a"},i(),n.components);return s.jsxs(s.Fragment,{children:[s.jsx(c,{of:x}),`
+`,s.jsx(e.h1,{id:"f-switch",children:"f-switch"}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"f-switch allows a user to turn an individual option true/false. The change in selection takes place immediately unlike checkboxes and radio buttons."}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"playground",children:s.jsx(e.a,{href:"/story/components-f-switch--playground",children:"Playground"})}),`
+`,s.jsx(t,{children:s.jsx(d,{of:j})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"anatomy",children:"Anatomy"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(d,{of:a}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"value",children:"value"}),`
+`,s.jsx(e.p,{children:"Value of a radio defines if it is selected, unselected."}),`
+`,s.jsx(t,{children:s.jsx(d,{of:o})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"true"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"false"}),s.jsx("td",{}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"size",children:"size"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"f-radio can have 2 sizes."}),`
+`,s.jsx(t,{children:s.jsx(d,{of:h})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"small"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"medium"}),s.jsx("td",{}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"state",children:"state"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,s.jsx(t,{children:s.jsx(d,{of:m})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"default"}),s.jsx("td",{}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"primary"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"success"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"danger"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"warning"}),s.jsx("td",{}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"flags",children:"Flags"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(t,{children:s.jsx(d,{of:u})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Name"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"disabled"}),s.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),s.jsx("td",{children:s.jsx("code",{children:"false"})})]})]})}),`
+`,s.jsx(l,{of:"f-switch"})]})}function k(n={}){const{wrapper:e}=Object.assign({},i(),n.components);return e?s.jsx(e,Object.assign({},n,{children:s.jsx(r,n)})):r(n)}export{k as default};
---
+++ assets/release-notes-2863350c.js
@@ -0,0 +1,96 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import"./blocks-2646952b.js";import{u as s}from"./index-a1cf9e47.js";import{M as i,e as r}from"./index-b213078e.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-c841b035.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";const a=`<h4 className="margin-btm-8">Release Notes</h4>
+
+# Change Log
+
+## [1.4.0] - 2024-11-21
+
+### Minor changes
+
+- new AZURE icons added
+
+## [1.3.0] - 2024-11-20
+
+### Minor changes
+
+- new GCP icons added
+
+## [1.2.2] - 2024-10-9
+
+### Improvement
+
+- Register function loads icons in parallel and returns the icon bundle as well
+
+## [1.2.1] - 2024-10-23
+
+### Minor changes
+
+- New icons added
+
+## [1.2.0] - 2024-10-01
+
+### Minor changes
+
+- New icons added
+
+## [1.1.0] - 2024-09-27
+
+### Minor changes
+
+- Only exports an ESM build. Any modern bundler in 2024 will work with it.
+
+## [1.0.2] - 2024-09-17
+
+### Patch
+
+- Icons updated
+
+## [1.0.1] - 2024-09-05
+
+### Patch
+
+- Policy icons import fixed.
+
+## [1.0.0] - 2024-09-04
+
+### Major
+
+- \`sass-system\` and \`engineering\` icons removed
+- bundle size reduced to 1.1MB from 3MB
+
+## [0.0.6] - 2024-08-29
+
+### Patch
+
+- icons updated
+
+## [0.0.5] - 2024-08-29
+
+### Patch
+
+- icons updated
+
+## [0.0.4] - 2024-08-28
+
+### Patch
+
+- icons updated
+
+## [0.0.3] - 2024-08-20
+
+### Patch
+
+- \`peerDependencies\` updated
+
+## [0.0.2] - 2024-08-07
+
+### Patch
+
+- \`peerDependencies\` updated
+
+## [0.0.1] - 2024-08-02
+
+### Patch
+
+- first release.
+`;function o(e){return n.jsxs(n.Fragment,{children:[n.jsx(i,{title:"@nonfx/flow-icons/Release Notes"}),`
+`,n.jsx(r,{children:a})]})}function b(e={}){const{wrapper:t}=Object.assign({},s(),e.components);return t?n.jsx(t,Object.assign({},e,{children:n.jsx(o,e)})):o()}export{b as default};
