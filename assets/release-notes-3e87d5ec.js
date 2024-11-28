9e6e88add674e6d0e6d7942bc102964801956d0e
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2024-11-28 11:19:16 +0000

    deploy: c822ca2715b4d26d0c182c78a80d0e1c24525282

---
+++ assets/release-notes-3e87d5ec.js
@@ -0,0 +1,23 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import"./blocks-2646952b.js";import{u as r}from"./index-a1cf9e47.js";import{M as i,e as s}from"./index-a4767dd9.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-b1a9c396.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";const a=`<h4 className="margin-btm-8">Release Notes</h4>
+
+# Change Log
+
+## [0.1.0] - 2024-09-27
+
+### Minor changes
+
+- Only exports an ESM build. Any modern bundler in 2024 will work with it.
+
+## [0.0.5] - 2024-07-31
+
+### Patch
+
+- \`f-text-editor\` : testing version control.
+
+## [0.0.1] - 2024-07-08
+
+### Features
+
+- \`f-text-editor\` : component added
+`;function o(t){return n.jsxs(n.Fragment,{children:[n.jsx(i,{title:"@nonfx/flow-text-editor/Release Notes"}),`
+`,n.jsx(s,{children:a})]})}function C(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(o,t)})):o()}export{C as default};
