90eb3bd7c673258f16ade276a2b7222c8dd10a8d
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2025-01-23 13:10:57 +0000

    deploy: d4dc2d12cb560f43f87e5d6de7afa178d068e3ca

---
+++ assets/f-icon-cebe32df.js
@@ -0,0 +1,39 @@
+import{j as s}from"./jsx-runtime-ffb262ed.js";import{M as c,C as n,b as d,A as l}from"./index-276c1fe6.js";import"./lit-html-b1a28dc3.js";import"./unsafe-svg-e29b2cc0.js";import{F as x,P as j,A as o,S as h,a,b as m,c as u}from"./f-icon.stories-588036d0.js";import{u as t}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-9f06f3ac.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./directive-12249aa5.js";function i(r){const e=Object.assign({h1:"h1",h2:"h2",p:"p",h3:"h3",a:"a"},t(),r.components);return s.jsxs(s.Fragment,{children:[s.jsx(c,{of:x}),`
+`,s.jsx(e.h1,{id:"f-icon",children:"f-icon"}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"Icons are used to provide visual context where written text is not required and enhance usability."}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"playground",children:s.jsx(e.a,{href:"/story/components-f-icon--playground",children:"Playground"})}),`
+`,s.jsx(n,{children:s.jsx(d,{of:j})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"anatomy",children:"Anatomy"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(d,{of:o}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"source",children:"source"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:`Source property defines what will be displayed on the icon. This could be an icon name from a
+library , any inline SVG or any URL for the image.`}),`
+`,s.jsx(n,{children:s.jsx(d,{of:h})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"Icon from library"}),s.jsx("td",{children:"e.g i-plus"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"Emoji as source"}),s.jsx("td",{children:"e.g. 😃"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"URL as source"}),s.jsx("td",{children:"url to be entered as source to see the icon image"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"size",children:"size"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"The small size is the default."}),`
+`,s.jsx(n,{children:s.jsx(d,{of:a})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"x-large"}),s.jsx("td",{children:"Extra large size - 36px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"large"}),s.jsx("td",{children:"Large size - 28px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"medium"}),s.jsx("td",{children:"Medium size - 20px"}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"small"}),s.jsx("td",{children:"Small size - 16px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"x-small"}),s.jsx("td",{children:"Extra Small size - 12px"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"state",children:"state"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"States on buttons are used to communicate purpose and it’s connotation."}),`
+`,s.jsx(n,{children:s.jsx(d,{of:m})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"default"}),s.jsx("td",{children:s.jsx("a",{children:"$color-icon-default"})}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"secondary"}),s.jsx("td",{children:s.jsx("a",{children:"$color-icon-secondary"})}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"subtle"}),s.jsx("td",{children:s.jsx("a",{children:"$color-icon-sublte"})}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"primary"}),s.jsx("td",{children:s.jsx("a",{children:"$color-success-default"})}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"danger"}),s.jsx("td",{children:s.jsx("a",{children:"$color-danger-default"})}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"warning"}),s.jsx("td",{children:s.jsx("a",{children:"$color-warning-default"})}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"inherit"}),s.jsx("td",{children:"inherit color according to parent f-div"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"custom"}),s.jsx("td",{children:"custom color can be given to the f-icon"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"flags",children:"Flags"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(n,{children:s.jsx(d,{of:u})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Name"}),s.jsx("td",{children:"Description"}),s.jsx("td",{children:"Default"})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"loading"}),s.jsx("td",{children:"Loader replaces the source"}),s.jsx("td",{children:"false"})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"disabled"}),s.jsx("td",{children:"Icon gets darker to demonstrate it's disability"}),s.jsx("td",{children:"false "})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"clickable"}),s.jsx("td",{children:'Icon on clickable="true" shows cursor:pointer and darkens a bit'}),s.jsx("td",{children:"false"})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"focused"}),s.jsx("td",{children:"Focused is a browser native property."}),s.jsx("td",{children:"false"})]})]})}),`
+`,s.jsx(l,{of:"f-icon"})]})}function N(r={}){const{wrapper:e}=Object.assign({},t(),r.components);return e?s.jsx(e,Object.assign({},r,{children:s.jsx(i,r)})):i(r)}export{N as default};
