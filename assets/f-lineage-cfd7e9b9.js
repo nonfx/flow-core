26fee535e45323650ab10e343cfc1538271cf24e
vikas-cldcvr <vikas-cldcvr@users.noreply.github.com>
2025-01-25 11:05:42 +0000

    deploy: 322192d8b977024c8064b66bc4c0253ce73b9292

---
+++ assets/f-lineage-cfd7e9b9.js
@@ -0,0 +1,10 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{M as i,C as s,b as m}from"./index-5ab5848e.js";import a from"./lineage-properties-77d5c2ca.js";import{F as p,P as c}from"./f-lineage.stories-f809e86d.js";import{u as t}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-6b5829cd.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-2bd2442b.js";import"./index-bbead140.js";import"./_defineProperty-5bd5182b.js";import"./index-b75c9059.js";import"./index-356e4a49.js";import"./index-1959b55b.js";import"./blocks-2646952b.js";import"./lit-html-b1a28dc3.js";import"./ref-a55570ea.js";import"./directive-helpers-7e9c1cf5.js";import"./directive-12249aa5.js";function r(o){const n=Object.assign({h1:"h1",h2:"h2",a:"a",p:"p"},t(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:p}),`
+`,e.jsx(n.h1,{id:"f-lineage",children:"f-lineage"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsxs(n.h2,{id:"overview--demo--sample-schema",children:["Overview | ",e.jsx(n.a,{href:"/story/components-f-lineage--playground",children:"Demo"})," | ",e.jsx("a",{href:"#lineage-sample",children:"Sample schema"})]}),`
+`,e.jsx(n.p,{children:"The lineage component is broken down into three parts : nodes, node children, and links. Checkout the properties below to configure them."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s,{children:e.jsx(m,{of:c})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(a,{})]})}function L(o={}){const{wrapper:n}=Object.assign({},t(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(r,o)})):r(o)}export{L as default};
