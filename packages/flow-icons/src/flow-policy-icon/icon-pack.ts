6243e6aa47c94e5486d97d168f7805f46bb69a65
Vikas <vikas.awaghade@hotmail.com>
2025-01-28 11:56:28 +0530

    icon-updates-5 icon updates

--- packages/flow-icons/src/flow-policy-icon/icon-pack.ts
+++ packages/flow-icons/src/flow-policy-icon/icon-pack.ts
@@ -1,11 +1,11 @@
-import i_policy_cis from "./svg/i-policy-cis";
 import i_policy_nist from "./svg/i-policy-nist";
-import i_policy_mastrm from "./svg/i-policy-mastrm";
-import i_policy_hipaa from "./svg/i-policy-hipaa";
-import i_policy_fca from "./svg/i-policy-fca";
-import i_policy_pci from "./svg/i-policy-pci";
+import i_policy_cis from "./svg/i-policy-cis";
 import i_policy_ojk from "./svg/i-policy-ojk";
+import i_policy_hipaa from "./svg/i-policy-hipaa";
+import i_policy_pci from "./svg/i-policy-pci";
 import i_policy_ISO27001 from "./svg/i-policy-ISO27001";
+import i_policy_mastrm from "./svg/i-policy-mastrm";
+import i_policy_fca from "./svg/i-policy-fca";
 import i_policy_soc2 from "./svg/i-policy-soc2";
 import i_policy_gdpr from "./svg/i-policy-gdpr";
 import p_aicpa from "./svg/p-aicpa";
@@ -15,20 +15,20 @@ import p_mitre from "./svg/p-mitre";
 import p_owasp from "./svg/p-owasp";
 import p_ffiec from "./svg/p-ffiec";
 import i_policy_bank_of_malaysia from "./svg/i-policy-bank-of-malaysia";
-import i_policy_sama from "./svg/i-policy-sama";
 import i_policy_arpa from "./svg/i-policy-arpa";
+import i_policy_sama from "./svg/i-policy-sama";
 import s_malaysia_gov from "./svg/s-malaysia-gov";
-import p_european_parliament from "./svg/p-european-parliament";
 import p_isaca from "./svg/p-isaca";
+import p_european_parliament from "./svg/p-european-parliament";
 const IconPack = {
-	"i-policy-cis": i_policy_cis,
 	"i-policy-nist": i_policy_nist,
-	"i-policy-mastrm": i_policy_mastrm,
-	"i-policy-hipaa": i_policy_hipaa,
-	"i-policy-fca": i_policy_fca,
-	"i-policy-pci": i_policy_pci,
+	"i-policy-cis": i_policy_cis,
 	"i-policy-ojk": i_policy_ojk,
+	"i-policy-hipaa": i_policy_hipaa,
+	"i-policy-pci": i_policy_pci,
 	"i-policy-ISO27001": i_policy_ISO27001,
+	"i-policy-mastrm": i_policy_mastrm,
+	"i-policy-fca": i_policy_fca,
 	"i-policy-soc2": i_policy_soc2,
 	"i-policy-gdpr": i_policy_gdpr,
 	"p-aicpa": p_aicpa,
@@ -38,10 +38,10 @@ const IconPack = {
 	"p-owasp": p_owasp,
 	"p-ffiec": p_ffiec,
 	"i-policy-bank-of-malaysia": i_policy_bank_of_malaysia,
-	"i-policy-sama": i_policy_sama,
 	"i-policy-arpa": i_policy_arpa,
+	"i-policy-sama": i_policy_sama,
 	"s-malaysia-gov": s_malaysia_gov,
-	"p-european-parliament": p_european_parliament,
-	"p-isaca": p_isaca
+	"p-isaca": p_isaca,
+	"p-european-parliament": p_european_parliament
 } as Record<string, string>;
 export default IconPack;
