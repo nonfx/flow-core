1a52f79a704bcb2c6c5f0d96683bbd8dcf047eee
NikhilVerma <NikhilVerma@users.noreply.github.com>
2025-02-26 05:11:32 +0000

    deploy: aff80b2b0612fcb617c05b3ab0c710df544997d3

---
+++ assets/f-text-editor-ChtbLjRY.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"./index-BjBhjGZp.js";import{M as i,C as s,S as d}from"./index-CaOiWeob.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import{F as x,P as m}from"./f-text-editor.stories-BvZiHNXB.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DVs4igA1.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./directive-CJw_OlP2.js";import"./ref-C9q2dBKz.js";import"./directive-helpers-CWXDk_SO.js";function n(r){const o={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...e(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:x}),`
+`,t.jsx(o.h1,{id:"f-text-editor",children:"f-text-editor"}),`
+`,t.jsx("br",{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"overview",children:"Overview"}),`
+`,t.jsx("f-divider",{}),`
+`,t.jsx(o.p,{children:"f-text-editor used to edit and view rich text content."}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h3,{id:"playground",children:t.jsx(o.a,{href:"/story/ollion-flow-text-editor-components-f-text-editor--playground",children:"Playground"})}),`
+`,t.jsx(s,{children:t.jsx(d,{of:m})}),`
+`,t.jsx("br",{})]})}function D(r={}){const{wrapper:o}={...e(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(n,{...r})}):n(r)}export{D as default};
