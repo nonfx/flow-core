da9861f35e1eecebebb295b0f038854b83ceaca3
shubhamseervi <shubhamseervi@users.noreply.github.com>
2025-03-04 12:57:52 +0000

    deploy: 1b5a3d5b64cd3579025702f58699dd1aca6245a6

---
+++ assets/login-form.stories-CpNUURsl.js
@@ -0,0 +1,200 @@
+import{x as c}from"./lit-html-BulfDIla.js";import{n as p,e as g}from"./ref-C9q2dBKz.js";import{v as o}from"./flow-form-builder.es-Cbz06X8C.js";import"./directive-helpers-CWXDk_SO.js";import"./directive-CJw_OlP2.js";import"./lit-element-3mBpJhh8.js";import"./property-Dn_e7HmW.js";import"./iframe-51hh6hxg.js";import"./index.es-DwvuBwXo.js";import"./unsafe-svg-Bgti8cH4.js";import"./repeat-DODqK66l.js";const q={title:"@nonfx/flow-form-builder/Examples",argTypes:{field:{control:!1}}},t={render:()=>{const e=g(),f={type:"text",label:{title:"Username"},validationRules:[{name:"required"}]},m={type:"password",label:{title:"Password"},validationRules:[{name:"required"},{name:"min",params:{length:6}}]},a=async()=>{if(e.value){const i=e.value.querySelector("[name='username']"),n=e.value.querySelector("[name='password']");return Promise.all([o(f,i,!1),o(m,n,!1)])}return[]},l=async()=>{if(e.value&&(await a()).every(n=>n.result)){const n=new FormData(e.value);console.log("call login api with these values",Object.fromEntries(n))}},u=i=>{i.key==="Enter"&&l()};return c`
+            <f-div align="middle-center" height="100%">
+                <form ${p(e)} @input=${a} @blur=${a} @keyup=${u}>
+                    <f-div
+                        direction="column"
+                        variant="curved"
+                        padding="x-large"
+                        gap="small"
+                        state="secondary"
+                        width="500px"
+                        height="hug-content"
+                    >
+                        <f-div align="middle-center" padding="none none large none">
+                            <f-text variant="heading" inline weight="bold" size="large">Log In</f-text>
+                        </f-div>
+                        <f-form-field>
+                            <f-div slot="label" padding="none" gap="none">Email</f-div>
+                            <f-input-light
+                                placeholder="Username"
+                                data-qa-element-id="username"
+                                autofocus
+                                name="username"
+                                autocomplete="on"
+                                clear
+                            ></f-input-light>
+                        </f-form-field>
+                        <f-form-field>
+                            <f-div slot="label" padding="none" gap="none">Password</f-div>
+                            <f-div slot="subtitle" padding="none" gap="none"
+                                ><f-text size="small">
+                                    <a href="#">Forgot password?</a>
+                                </f-text></f-div
+                            >
+                            <f-input-light
+                                placeholder="Password"
+                                type="password"
+                                data-qa-element-id="userPassword"
+                                name="password"
+                                autocomplete="on"
+                                clear
+                            ></f-input-light>
+                        </f-form-field>
+                        <f-div padding="small" align="middle-center" gap="medium">
+                            <f-button @click=${l} style="width:150px" label="log in"></f-button>
+                            <f-button
+                                style="width:150px"
+                                label="sign up"
+                                category="outline"
+                                state="neutral"
+                            ></f-button>
+                        </f-div>
+                        <f-div direction="column" gap="medium" padding="small" align="middle-center">
+                            <f-text size="small">Or, log in with</f-text>
+                            <f-div align="middle-center" gap="medium" height="28px">
+                                <f-icon source="p-google" clickable size="large"></f-icon>
+                                <f-divider></f-divider>
+                                <f-icon source="p-azure" clickable size="large"></f-icon>
+                                <f-divider></f-divider>
+                                <f-icon source="p-github" clickable size="large"></f-icon>
+                            </f-div>
+                        </f-div>
+                    </f-div>
+                </form>
+            </f-div>
+        `},name:"Login Form Without Form Builder"};var r,d,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => {
+    const formRef = createRef<HTMLFormElement>();
+    /**
+     * Define username field with validation rules
+     */
+    const usernameField: FormBuilderTextInputField = {
+      type: "text",
+      label: {
+        title: "Username"
+      },
+      validationRules: [{
+        name: "required"
+      }]
+    };
+
+    /**
+     * Define password field with validation rules
+     */
+    const passwordField: FormBuilderTextInputField = {
+      type: "password",
+      label: {
+        title: "Password"
+      },
+      validationRules: [{
+        name: "required"
+      }, {
+        name: "min",
+        params: {
+          length: 6
+        }
+      }]
+    };
+    /**
+     * validates form
+     */
+    const validateForm = async () => {
+      if (formRef.value) {
+        const usernameElement = formRef.value.querySelector("[name='username']") as FInputLight;
+        const passwordElement = formRef.value.querySelector("[name='password']") as FInputLight;
+        return Promise.all([validateField(usernameField, usernameElement, false), validateField(passwordField, passwordElement, false)]);
+      }
+      return [];
+    };
+
+    /**
+     * validate and call login api
+     */
+    const login = async () => {
+      if (formRef.value) {
+        const results = await validateForm();
+        if (results.every(r => r.result)) {
+          const formData = new FormData(formRef.value);
+          console.log("call login api with these values", Object.fromEntries(formData));
+        }
+      }
+    };
+    /**
+     *  Check if Enter button is pressed
+     */
+    const checkSubmit = (e: KeyboardEvent) => {
+      if (e.key === "Enter") {
+        login();
+      }
+    };
+    /**
+     * Login template
+     */
+    return html\`
+            <f-div align="middle-center" height="100%">
+                <form \${ref(formRef)} @input=\${validateForm} @blur=\${validateForm} @keyup=\${checkSubmit}>
+                    <f-div
+                        direction="column"
+                        variant="curved"
+                        padding="x-large"
+                        gap="small"
+                        state="secondary"
+                        width="500px"
+                        height="hug-content"
+                    >
+                        <f-div align="middle-center" padding="none none large none">
+                            <f-text variant="heading" inline weight="bold" size="large">Log In</f-text>
+                        </f-div>
+                        <f-form-field>
+                            <f-div slot="label" padding="none" gap="none">Email</f-div>
+                            <f-input-light
+                                placeholder="Username"
+                                data-qa-element-id="username"
+                                autofocus
+                                name="username"
+                                autocomplete="on"
+                                clear
+                            ></f-input-light>
+                        </f-form-field>
+                        <f-form-field>
+                            <f-div slot="label" padding="none" gap="none">Password</f-div>
+                            <f-div slot="subtitle" padding="none" gap="none"
+                                ><f-text size="small">
+                                    <a href="#">Forgot password?</a>
+                                </f-text></f-div
+                            >
+                            <f-input-light
+                                placeholder="Password"
+                                type="password"
+                                data-qa-element-id="userPassword"
+                                name="password"
+                                autocomplete="on"
+                                clear
+                            ></f-input-light>
+                        </f-form-field>
+                        <f-div padding="small" align="middle-center" gap="medium">
+                            <f-button @click=\${login} style="width:150px" label="log in"></f-button>
+                            <f-button
+                                style="width:150px"
+                                label="sign up"
+                                category="outline"
+                                state="neutral"
+                            ></f-button>
+                        </f-div>
+                        <f-div direction="column" gap="medium" padding="small" align="middle-center">
+                            <f-text size="small">Or, log in with</f-text>
+                            <f-div align="middle-center" gap="medium" height="28px">
+                                <f-icon source="p-google" clickable size="large"></f-icon>
+                                <f-divider></f-divider>
+                                <f-icon source="p-azure" clickable size="large"></f-icon>
+                                <f-divider></f-divider>
+                                <f-icon source="p-github" clickable size="large"></f-icon>
+                            </f-div>
+                        </f-div>
+                    </f-div>
+                </form>
+            </f-div>
+        \`;
+  },
+  name: "Login Form Without Form Builder"
+}`,...(s=(d=t.parameters)==null?void 0:d.docs)==null?void 0:s.source}}};const L=["LoginForm"];export{t as LoginForm,L as __namedExportsOrder,q as default};
