a2931a1aef044272cdda6180496fec717deb6f40
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-03-21 14:06:55 +0000

    deploy: 050c74b2f49942dc7252c4bab8342072fe668b78

---
+++ assets/f-countdown-Dl-d9Ujd.js
@@ -0,0 +1,44 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-BjBhjGZp.js";import{M as l,C as n,S as e}from"./index-C_GpSLdt.js";import{F as c,P as x,D as j,C as h,S as o,a,L as p}from"./f-countdown.stories-pVulrb2N.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BrzvSh6f.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";function t(r){const d={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...i(),...r.components};return s.jsxs(s.Fragment,{children:[s.jsx(l,{of:c}),`
+`,s.jsx(d.h1,{id:"f-countdown",children:"f-countdown"}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(d.h2,{id:"overview",children:"Overview"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(d.p,{children:"Breadcrumbs are secondary navigations which shows hierarchy for a user’s location within an app/product."}),`
+`,s.jsx("br",{}),`
+`,s.jsx(d.h3,{id:"playground",children:s.jsx(d.a,{href:"/story/components-f-breadcrumb--playground",children:"Playground"})}),`
+`,s.jsx(n,{children:s.jsx(e,{of:x})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(d.h2,{id:"duration",children:"duration"}),`
+`,s.jsx(d.p,{children:"Duration for f-countdown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:j})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(d.h2,{id:"category",children:"category"}),`
+`,s.jsx(d.p,{children:"Category for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:h})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"fill"}),s.jsx("td",{children:"background is filled with respective state"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(d.h2,{id:"size",children:"size"}),`
+`,s.jsx(d.p,{children:"Sizes for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:o})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"large"}),s.jsx("td",{children:"44px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"medium"}),s.jsx("td",{children:"36px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"small"}),s.jsx("td",{children:"28px"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"x-small"}),s.jsx("td",{children:"20px"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(d.h2,{id:"state",children:"state"}),`
+`,s.jsx(d.p,{children:"States for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:a})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"default"}),s.jsx("td",{children:"--color-primary-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"primary"}),s.jsx("td",{children:"--color-primary-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"success"}),s.jsx("td",{children:"--color-success-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"warning"}),s.jsx("td",{children:"--color-warning-default"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"danger"}),s.jsx("td",{children:"--color-danger-default"}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(d.h2,{id:"label-placement",children:"label-placement"}),`
+`,s.jsx(d.p,{children:"label-placement for f-coundown."}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(n,{children:s.jsx(e,{of:p})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"top"}),s.jsx("td",{children:"label placed at top"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"left"}),s.jsx("td",{children:"label placed at left"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"bottom"}),s.jsx("td",{children:"label placed at bottom"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"right"}),s.jsx("td",{children:"label placed at right"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"none"}),s.jsx("td",{children:"label is hidden"}),s.jsx("td",{})]})]})})]})}function M(r={}){const{wrapper:d}={...i(),...r.components};return d?s.jsx(d,{...r,children:s.jsx(t,{...r})}):t(r)}export{M as default};
---
+++ assets/f-form-group-BYBZKmMf.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-BjBhjGZp.js";import{M as c,C as t,S as r}from"./index-C_GpSLdt.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import{F as o,P as x,V as h,D as a,G as j,C as p,a as m}from"./f-form-group.stories-BhqQyZ00.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BrzvSh6f.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./directive-CJw_OlP2.js";function i(n){const s={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...l(),...n.components},{ArgsTable:d}=s;return d||f("ArgsTable"),e.jsxs(e.Fragment,{children:[e.jsx(c,{of:o}),`
+`,e.jsx(s.h1,{id:"f-form-group",children:"f-form-group"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"Use f-form-group to create logical sections within the form."}),`
+`,e.jsx(s.p,{children:"f-form-group is used to group multiple instances of an input element."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-form-group--playground",children:"Playground"})}),`
+`,e.jsx(t,{children:e.jsx(r,{of:x})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"Variant decides whether the input elements in a group have some gap or not when they are aligned horizontally."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(s.p,{children:"Note: If variant is normal then the gap between elements is controlled by the parent group."})}),`
+`,e.jsx(t,{children:e.jsx(r,{of:h})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"normal"}),e.jsx("td",{}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"compact"}),e.jsx("td",{children:"gap between elements is 0"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"direction",children:"direction"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"Decides the direction of the input elements within the group."}),`
+`,e.jsx(t,{children:e.jsx(r,{of:a})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"vertical"}),e.jsx("td",{}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"horizontal"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"gap",children:"gap"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"decides the gap between elements of a group"}),`
+`,e.jsx(t,{children:e.jsx(r,{of:j})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"large"}),e.jsx("td",{children:"Items take a space gap of 16px in between"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:"Items take a space gap of 12px in between"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:"Items take a space gap of 8px in between"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"x-small"}),e.jsx("td",{children:"Items take a space gap of 4px in between"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"collapse",children:"collapse"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"Defines whether the group will be collapsed as an accordion or as text."}),`
+`,e.jsx(t,{children:e.jsx(r,{of:p})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"none"}),e.jsx("td",{children:"padding of 24px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"accordion"}),e.jsx("td",{children:"padding of 16px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"text"}),e.jsx("td",{children:"padding of 12px"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"flags",children:"Flags"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t,{children:e.jsx(r,{of:m})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Name"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"can-duplicate"}),e.jsx("td",{children:"Present if user wants to duplicate the group. Clicking on this button emits an event"}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]})]})}),`
+`,e.jsx(d,{of:"f-form-group"})]})}function I(n={}){const{wrapper:s}={...l(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(i,{...n})}):i(n)}function f(n,s){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{I as default};
