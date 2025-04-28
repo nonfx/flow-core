089834a5c7e55f9b00e035ae660188bc30671c3e
shubhamseervi <shubhamseervi@users.noreply.github.com>
2025-04-28 12:23:18 +0000

    deploy: 01844306ffa4d7c0de9075b02d52f6c77ddf38e1

---
+++ assets/About-Vg-iaLsC.js
@@ -0,0 +1,29 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"./index-BjBhjGZp.js";import{M as i}from"./index-HGt3iiUe.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-0I_2Exan.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";const s=`# Flow Icons
+
+The Flow Icons is built on the Flow design framework
+
+# Installation
+
+### 1️⃣ Install flow icons dependency
+
+\`\`\`
+npm i --save @nonfx/flow-icons
+\`\`\`
+
+**Note:** after installation, re-start your application.
+
+<br>
+
+### 2️⃣ Import flow-icons into your project
+
+Paste the below snippet in your project and add your application startup/runtime code to it.
+
+\`\`\`javascript
+import "@nonfx/flow-core";
+import { register } from "@nonfx/flow-icons";
+register(["system"]);
+\`\`\`
+
+<br>
+`;function r(o){return n.jsxs(n.Fragment,{children:[n.jsx(i,{title:"@nonfx/flow-icons/About"}),`
+`,n.jsx("f-md-editor",{value:s})]})}function w(o={}){const{wrapper:t}={...e(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(r,{...o})}):r()}export{w as default};
---
+++ assets/f-divider-Bn26GmES.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as o}from"./index-BjBhjGZp.js";import{M as l,C as n,S as i}from"./index-HGt3iiUe.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import{F as c,P as x,A as a,S as j,V as h,a as f,b as p}from"./f-divider.stories-BsNK9xYO.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-0I_2Exan.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./directive-CJw_OlP2.js";function t(d){const s={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...o(),...d.components},{ArgsTable:r}=s;return r||u("ArgsTable"),e.jsxs(e.Fragment,{children:[e.jsx(l,{of:c}),`
+`,e.jsx(s.h1,{id:"f-divider",children:"f-divider"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-divider is a line used to separate the content of a layout."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-divider--playground",children:"Playground"})}),`
+`,e.jsx(n,{children:e.jsx(i,{of:x})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"anatomy",children:"Anatomy"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(i,{of:a}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"specifications",children:"Specifications"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:`A divider can be horizontal or vertical depending on its parent’s layout. In a row layout a
+divider is vertical. In a column layout it is horizontal.`}),`
+`,e.jsx("br",{}),`
+`,e.jsx(i,{of:j}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-divider can have 3 variants which are visual styles: solid, dashed, dotted"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n,{children:e.jsx(i,{of:h})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"solid"}),e.jsx("td",{children:"solid visual style"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"dotted"}),e.jsx("td",{children:"dotted visual style"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"dashed"}),e.jsx("td",{children:"dashed visual style"}),e.jsx("td",{})]})]})}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Size property defines the thickness of the divider."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n,{children:e.jsx(i,{of:f})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"large"}),e.jsx("td",{children:"size is set to be 2px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:"size is set to be 1px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"state",children:"state"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"States on buttons are used to communicate purpose and it’s connotation."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n,{children:e.jsx(i,{of:p})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"default"}),e.jsx("td",{children:e.jsx("a",{children:"$color-border-default"})}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"secondary"}),e.jsx("td",{children:e.jsx("a",{children:"$color-border-secondary"})}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"subtle"}),e.jsx("td",{children:e.jsx("a",{children:"$color-border-subtle"})}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"custom"}),e.jsx("td",{children:"custom color can be given to the f-divider"}),e.jsx("td",{})]})]})}),`
+`,e.jsx(r,{of:"f-divider"})]})}function E(d={}){const{wrapper:s}={...o(),...d.components};return s?e.jsx(s,{...d,children:e.jsx(t,{...d})}):t(d)}function u(d,s){throw new Error("Expected component `"+d+"` to be defined: you likely forgot to import, pass, or provide it.")}export{E as default};
