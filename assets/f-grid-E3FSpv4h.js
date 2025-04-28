a4dc60c0dc92fe05acb0ff3f1862f31465aa690b
shubhamseervi <shubhamseervi@users.noreply.github.com>
2025-04-28 08:57:55 +0000

    deploy: b62986bf2b1a9f1ec73a69df3faf2f24ad3fc840

---
+++ assets/f-grid-E3FSpv4h.js
@@ -0,0 +1,45 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-BjBhjGZp.js";import{M as a,C as t,S as i}from"./index-De9Hv4hx.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import{F as c,P as o,A as x,M as h,a as j,C as p,G as m}from"./f-grid.stories-C8JE7Snu.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-g9J8NWaQ.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./directive-CJw_OlP2.js";import"./chunk-2WNKQWTL-cTwTzLwY.js";function d(s){const n={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...l(),...s.components},{ArgsTable:r}=n;return r||f("ArgsTable"),e.jsxs(e.Fragment,{children:[e.jsx(a,{of:c}),`
+`,e.jsx(n.h1,{id:"f-grid",children:"f-grid"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:"f-grid is a wrapper component to arrange data as a grid of cells. The f-divs wrapped inside f-grid are called cells."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"playground",children:e.jsx(n.a,{href:"/story/components-f-grid--playground",children:"Playground"})}),`
+`,e.jsx(t,{children:e.jsx(i,{of:o})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"anatomy",children:"Anatomy"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(i,{of:x}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"min-cell-width",children:"min-cell-width"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:"Defines the minimum value of width of each cell. A cell can not be smaller than this in any viewport."}),`
+`,e.jsx("p",{className:"note-section",children:"Note: The min-cell-width can not take value in fraction or percent."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t,{children:e.jsx(i,{of:h})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"max-cell-width",children:"max-cell-width"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:"Defines the maximum width of each cell."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(n.p,{children:"Note: max-cell-width accept number, percentage, or fraction as values."})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t,{children:e.jsx(i,{of:j})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"cell-height",children:"cell-height"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:"Defines the height of each cell"}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(n.p,{children:"Note: max-cell-width accept number, percentage, or fraction as values."})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t,{children:e.jsx(i,{of:p})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"gap",children:"gap"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:"Defines the gap between the cells. By default it is set to small."}),`
+`,e.jsx("p",{className:"note-section",children:"Note: gap property defines horizontal as well as vertical gap."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t,{children:e.jsx(i,{of:m})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"x-large"}),e.jsx("td",{children:"Items take a space gap of 32px in between"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"large"}),e.jsx("td",{children:"Items take a space gap of 24px in between"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:"Items take a space gap of 16px in between"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:"Items take a space gap of 12px in between"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"x-small"}),e.jsx("td",{children:"Items take a space gap of 8px in between"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(r,{of:"f-grid"})]})}function E(s={}){const{wrapper:n}={...l(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(d,{...s})}):d(s)}function f(s,n){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{E as default};
