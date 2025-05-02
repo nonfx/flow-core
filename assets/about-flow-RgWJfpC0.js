adf141145aaedff82cc8fe0e7632d8bdc378515e
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-05-02 06:16:10 +0000

    deploy: 4f43692cab7c2a29639bd5ede16196210f089c73

---
+++ assets/about-flow-RgWJfpC0.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as t}from"./index-BjBhjGZp.js";import{M as s}from"./index-Co9D3jHO.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Baqu-TR4.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function i(o){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...t(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"About"}),`
+`,e.jsx(n.h1,{id:"welcome-to-flow-",children:"Welcome to Flow 🎉"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h2,{id:"what-is-flow",children:"What is Flow?"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:"A design system that consists of principles, patterns, guidelines, and processes to enable design at scale; without compromising on the developer experience and design standards of CloudCover."}),`
+`,e.jsx(n.h2,{id:"key-features",children:"Key Features"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"framework-agnostic",children:"Framework Agnostic"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsxs(n.p,{children:["Flow works on any HTML or JS script based frameworks."," "]}),`
+`,e.jsx(n.h3,{id:"obvious",children:"Obvious"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:"Everything we build and design needs to be simple, intuitive, and obvious to anyone using the framework. This includes naming conventions, props and values definitions, documentation, etc."}),`
+`,e.jsx(n.h3,{id:"figma--front-end",children:"Figma → Front-end"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsxs(n.p,{children:[`Everything you see in Flow from a front-end implementation POV has to exist in Figma. The Figma
+component properties or template layouts have to be the same for the developer experience. `,e.jsx("br",{})," "]}),`
+`,e.jsx("br",{}),`
+`,e.jsxs(n.p,{children:[`If you are a developer with a new contribution to Flow and do not have Figma experience/skills, you
+can write into `,e.jsx("a",{href:"https://flow.ollion.com/",target:"blank",children:`
+flow@nonfx.com
+`}),` and someone from the CloudCover’s design team will get in touch with you. Keep in mind that the
+designers are actively engaged into other products and a response/implementation might take a little
+time.`]}),`
+`,e.jsx(n.h3,{id:"dependencies",children:"Dependencies"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n.p,{children:`To keep everything lean and customizable, any new features should be created as dependencies to
+avoid bloat at the Flow core level. This allows for features to live on different streams and
+continuously develop/update without affecting the main core.`})]})}function f(o={}){const{wrapper:n}={...t(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(i,{...o})}):i(o)}export{f as default};
