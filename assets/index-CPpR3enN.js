279a7d44b08b6a43b81fa2e718588ebc79e4f340
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-05-06 08:31:52 +0000

    deploy: 85d353c555733bb168155c89119211f9a58c9f55

---
+++ assets/index-CPpR3enN.js
@@ -0,0 +1,1 @@
+import{C as o}from"./index.es-DwvuBwXo.js";import{I as i}from"./icon-pack-DyY45-Zz.js";o.setConfig({iconPack:{...i,...o.getConfig().iconPack}});export{i as default};
--- assets/index-R82ePzFc.js
+++
@@ -1,1 +0,0 @@
-import{C as o}from"./index.es-DwvuBwXo.js";import{I as i}from"./icon-pack-Dne-HmB8.js";o.setConfig({iconPack:{...i,...o.getConfig().iconPack}});export{i as default};
