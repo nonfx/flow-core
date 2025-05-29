f786078dbc827e7553db9c48458ee7a9ca2f922d
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-05-29 05:53:12 +0000

    deploy: 4dbe296937f5346f3643a7a1ca0f2dafa8a06648

---
+++ assets/f-tab-Bc-TxgjM.js
@@ -0,0 +1,39 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-BjBhjGZp.js";import{M as o,C as r,S as i}from"./index-Co0_hMnm.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import{F as a,P as c,V as h,D as x,N as j,A as b}from"./f-tab.stories-C21fEPR-.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-QpScSlt6.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./directive-CJw_OlP2.js";import"./chunk-2WNKQWTL-cTwTzLwY.js";function d(t){const e={a:"a",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...l(),...t.components},{ArgsTable:s}=e;return s||p("ArgsTable"),n.jsxs(n.Fragment,{children:[n.jsx(o,{of:a}),`
+`,n.jsx(e.h1,{id:"f-tab",children:"f-tab"}),`
+`,n.jsx("br",{}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx("hr",{}),`
+`,n.jsx(e.p,{children:"Tabs are broken into two parts. They are connected by a matching content-[id] for example. Tab node (tab-node-1) : Tab content (tab-content-1)"}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h3,{id:"playground",children:n.jsx(e.a,{href:"/story/components-f-tab--playground",children:"Playground"})}),`
+`,n.jsx(r,{children:n.jsx(i,{of:c})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"variant",children:"variant"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"variant of every tab node"}),`
+`,n.jsx(r,{children:n.jsx(i,{of:h})}),`
+`,n.jsx("table",{className:"custom-table",children:n.jsxs("tbody",{children:[n.jsxs("tr",{children:[n.jsx("td",{children:"Value"}),n.jsx("td",{children:"Description"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"fill"}),n.jsx("td",{children:"takes up $surface-secondary color as background"}),n.jsx("td",{children:n.jsx("code",{children:"default"})})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"transparent"}),n.jsx("td",{children:"transparent background"}),n.jsx("td",{})]})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"direction",children:"direction"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Vertical tabs can be enabled using direction property. By default tabs are horizontal."}),`
+`,n.jsx(r,{children:n.jsx(i,{of:x})}),`
+`,n.jsx("table",{className:"custom-table",children:n.jsxs("tbody",{children:[n.jsxs("tr",{children:[n.jsx("td",{children:"Value"}),n.jsx("td",{children:"Description"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"horizontal"}),n.jsx("td",{children:"horizontally aligned tabs"}),n.jsx("td",{children:n.jsx("code",{children:"default"})})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"vertical"}),n.jsx("td",{children:"vertic aligned tabs"}),n.jsx("td",{})]})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"node-width",children:"node-width"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Defines the width of the tab nodes."}),`
+`,n.jsx(r,{children:n.jsx(i,{of:j})}),`
+`,n.jsx("table",{className:"custom-table",children:n.jsxs("tbody",{children:[n.jsxs("tr",{children:[n.jsx("td",{children:"Value"}),n.jsx("td",{children:"Description"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"fill"}),n.jsx("td",{children:"Equal width, filling parent container"}),n.jsx("td",{children:n.jsx("code",{children:"default"})})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"hug-content"}),n.jsx("td",{children:"Variable width"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"num value"}),n.jsx("td",{children:"Fixed width, user defined "}),n.jsx("td",{})]})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"alignment",children:"alignment"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"The alignment property aligns a f-tab bar with respect to the parent container."}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"For horizontal f-tabs, content can align to left, center, or right."}),`
+`,n.jsx(e.li,{children:"For vertical f-tabs, content can align to top, middle, or bottom."}),`
+`]}),`
+`,n.jsx(r,{children:n.jsx(i,{of:b})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(s,{of:"f-tab"})]})}function N(t={}){const{wrapper:e}={...l(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(d,{...t})}):d(t)}function p(t,e){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{N as default};
