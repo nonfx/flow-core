96871c8ca5f5e9b499481e6e4a53b436fc3534d3
NikhilVerma <NikhilVerma@users.noreply.github.com>
2025-05-29 10:06:58 +0000

    deploy: 367492e75442580623158a880ae9f5977a264116

---
+++ assets/f-checkbox-D7l7n7es.js
@@ -0,0 +1,38 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as c}from"./index-BjBhjGZp.js";import{M as x,C as t,S as d}from"./index-IHZOsnxc.js";import{F as l,P as o,A as j,V as a,S as h,a as m,b as p}from"./f-checkbox.stories-BBHZ9snq.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CtKCMm7E.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import"./directive-CJw_OlP2.js";import"./chunk-2WNKQWTL-cTwTzLwY.js";function i(n){const e={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...c(),...n.components},{ArgsTable:r}=e;return r||f("ArgsTable"),s.jsxs(s.Fragment,{children:[s.jsx(x,{of:l}),`
+`,s.jsx(e.h1,{id:"f-checkbox",children:"f-checkbox"}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"f-checkbox allows a user to mark one individual item as selected, or to select multiple items from a list of individual items."}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"playground",children:s.jsx(e.a,{href:"/story/components-f-checkbox--playground",children:"Playground"})}),`
+`,s.jsx(t,{children:s.jsx(d,{of:o})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"anatomy",children:"Anatomy"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(d,{of:j}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"value",children:"value"}),`
+`,s.jsx(e.p,{children:"Value of a checkbox defines if it is selected, unselected or indeterminate."}),`
+`,s.jsx(t,{children:s.jsx(d,{of:a})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"checked"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"unchecked"}),s.jsx("td",{}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"indeterminate"}),s.jsx("td",{}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"size",children:"size"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"f-checkbox can have 2 sizes."}),`
+`,s.jsx(t,{children:s.jsx(d,{of:h})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"small"}),s.jsx("td",{children:'size="12px"'}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"medium"}),s.jsx("td",{children:'size="16px"'}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"state",children:"state"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,s.jsx(t,{children:s.jsx(d,{of:m})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"default"}),s.jsx("td",{}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"primary"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"success"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"danger"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"warning"}),s.jsx("td",{}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"flags",children:"Flags"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(t,{children:s.jsx(d,{of:p})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Name"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"disabled"}),s.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),s.jsx("td",{children:s.jsx("code",{children:"false"})})]})]})}),`
+`,s.jsx(r,{of:"f-checkbox"})]})}function A(n={}){const{wrapper:e}={...c(),...n.components};return e?s.jsx(e,{...n,children:s.jsx(i,{...n})}):i(n)}function f(n,e){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{A as default};
