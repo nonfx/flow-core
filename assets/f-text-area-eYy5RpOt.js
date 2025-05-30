9f9df2014de5484a2a20528cfa53f07e53cae7c8
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-05-30 06:38:16 +0000

    deploy: 47b7117afe4ec2e2a428dff60ace77e013d94a17

---
+++ assets/f-text-area-eYy5RpOt.js
@@ -0,0 +1,62 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-BjBhjGZp.js";import{M as a,C as t,S as n}from"./index-MKSmK206.js";import{F as x,P as c,A as o,C as h,V as j,a as f,S as p,b as m,M as u,R as b,c as y}from"./f-text-area.stories-B7VLb1Zd.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DsTfs-8E.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import"./directive-CJw_OlP2.js";function i(r){const s={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...l(),...r.components},{ArgsTable:d}=s;return d||g("ArgsTable"),e.jsxs(e.Fragment,{children:[e.jsx(a,{of:x}),`
+`,e.jsx(s.h1,{id:"f-text-area",children:"f-text-area"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-text-area allows the user to input a longer amount of text than f-input. It is commonly useful for descriptions and user feedback."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-text-area--playground",children:"Playground"})}),`
+`,e.jsx(t,{children:e.jsx(n,{of:c})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"anatomy",children:"Anatomy"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n,{of:o}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"category",children:"category"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Category decides the background of f-text-area"}),`
+`,e.jsx(t,{children:e.jsx(n,{of:h})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"fill"}),e.jsx("td",{children:"background is filled"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"outline"}),e.jsx("td",{children:"empty background with border"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"transparent"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"value",children:"value"}),`
+`,e.jsx(s.p,{children:"Defines the value of an f-text-area."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:j})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"placeholder",children:"placeholder"}),`
+`,e.jsx(s.p,{children:"Defines the placeholder text for f-text-area"}),`
+`,e.jsx(t,{children:e.jsx(n,{of:f})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-text-area can have 2 sizes."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(s.p,{children:`Note: Font styles are same in both sizes. Only paddings and gaps are different. By default size is
+inherited by the parent f-form.`})}),`
+`,e.jsx(t,{children:e.jsx(n,{of:p})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"state",children:"state"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:m})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"default"}),e.jsx("td",{}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"primary"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"success"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"danger"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"warning"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"max-length",children:"max-length"}),`
+`,e.jsx(s.p,{children:"This shows the character count while typing and auto limits after reaching the max length."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:u})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"rows",children:"rows"}),`
+`,e.jsx(s.p,{children:"Defines the no. of rows to display. By default f-text-area provides 3 rows. After 3 rows text area becomes scrollable."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(s.p,{children:"Note: row property controls height. Width of text-area is always fill-container"})}),`
+`,e.jsx(t,{children:e.jsx(n,{of:b})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"flags",children:"Flags"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t,{children:e.jsx(n,{of:y})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Name"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"resizable"}),e.jsx("td",{children:e.jsx(s.p,{children:`Provides a resize handle on the bottom right of text-area which enables a user to resize the
+text-area within the parents scope. Note: text-area can be resized only vertically.`})}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"disabled"}),e.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"clear"}),e.jsx("td",{children:e.jsx(s.p,{children:`Displays a close icon-button on the right side of the input that allows the user to clear
+the input value`})}),e.jsx("td",{children:e.jsx("code",{children:"true"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"read-only"}),e.jsx("td",{children:"Only Text could be read, can't be edited"}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"mask-value"}),e.jsx("td",{children:"text will be masked with dots"}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(d,{of:"f-text-area"})]})}function V(r={}){const{wrapper:s}={...l(),...r.components};return s?e.jsx(s,{...r,children:e.jsx(i,{...r})}):i(r)}function g(r,s){throw new Error("Expected component `"+r+"` to be defined: you likely forgot to import, pass, or provide it.")}export{V as default};
