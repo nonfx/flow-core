5a01d4ccbc77ce861b1dc5b2f9ebc6d13aa74bed
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-06-13 13:51:58 +0000

    deploy: af0c3a047a9f132c5fec8a4458b1dfcab7cb081c

---
+++ assets/f-tab-node-BkVgC2W8.js
@@ -0,0 +1,25 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as d}from"./index-BjBhjGZp.js";import{M as a,C as r,S as t}from"./index-BwoS1CE1.js";import{F as c,P as x,C as h,A as j,D as l}from"./f-tab-node.stories-BQXDjTeO.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BSsO0Jck.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";import"./ref-C9q2dBKz.js";import"./directive-helpers-CWXDk_SO.js";import"./directive-CJw_OlP2.js";function s(o){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...d(),...o.components},{ArgsTable:i}=e;return i||p("ArgsTable"),n.jsxs(n.Fragment,{children:[n.jsx(a,{of:c}),`
+`,n.jsx(e.h1,{id:"f-tab-node",children:"f-tab-node"}),`
+`,n.jsx("br",{}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx("hr",{}),`
+`,n.jsx(e.p,{children:"f-tab-node holds the content of every tab."}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h3,{id:"playground",children:n.jsx(e.a,{href:"/story/components-f-tab-node--playground",children:"Playground"})}),`
+`,n.jsx(r,{children:n.jsx(t,{of:x})}),`
+`,n.jsx(e.h2,{id:"content-id",children:"content-id"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsxs(e.p,{children:[n.jsx(e.code,{children:"id"})," to match up with ",n.jsx(e.code,{children:"f-tab-node"})," ",n.jsx(e.code,{children:"content-id"}),"."]}),`
+`,n.jsx(r,{children:n.jsx(t,{of:h})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"active",children:"active"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"Blue bottom border appears for the active node. Takes up boolean value."}),`
+`,n.jsx(r,{children:n.jsx(t,{of:j})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"disabled",children:"disabled"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(e.p,{children:"To disable f-tab-node"}),`
+`,n.jsx(r,{children:n.jsx(t,{of:l})}),`
+`,n.jsx(i,{of:"f-tab-node"})]})}function F(o={}){const{wrapper:e}={...d(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(s,{...o})}):s(o)}function p(o,e){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{F as default};
--- assets/index-CLOLOtNe.js
+++
@@ -1,1 +0,0 @@
-import{C as o}from"./index.es-DwvuBwXo.js";import{I as i}from"./icon-pack-C-6E2B92.js";o.setConfig({iconPack:{...i,...o.getConfig().iconPack}});export{i as default};
