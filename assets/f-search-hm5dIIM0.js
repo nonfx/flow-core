e0087b8920d2ba29335fa8a9871d548a9ea250b8
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-06-17 06:31:09 +0000

    deploy: ebb0f6aefb1beee1f086d06956ffb144d7208468

---
+++ assets/f-search-hm5dIIM0.js
@@ -0,0 +1,61 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as c}from"./index-BjBhjGZp.js";import{M as l,C as r,S as n}from"./index-EhmjAGC5.js";import{F as x,P as h,V as o,C as j,a,b as p,S as f,c as u,R as b,d as m,e as g}from"./f-search.stories-RFobhDAI.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-z53kX22p.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";function i(d){const s={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...c(),...d.components},{ArgsTable:t}=s;return t||y("ArgsTable"),e.jsxs(e.Fragment,{children:[e.jsx(l,{of:x}),`
+`,e.jsx(s.h1,{id:"f-search",children:"f-search"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Search enables users to specify a word or a phrase to find relevant pieces of content."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-search--playground",children:"Playground"})}),`
+`,e.jsx(r,{children:e.jsx(n,{of:h})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Variants are various visual representations of an input field."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:o})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"curved"}),e.jsx("td",{children:"border radius of 4px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"round"}),e.jsx("td",{children:"border radius of 22px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"block"}),e.jsx("td",{children:'works well with cateogry="transparent"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"category",children:"category"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Category decides the background of f-search"}),`
+`,e.jsx(r,{children:e.jsx(n,{of:j})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"fill"}),e.jsx("td",{children:"background is filled"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"outline"}),e.jsx("td",{children:"empty background with border"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"transparent"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"value",children:"value"}),`
+`,e.jsx(s.p,{children:"Defines the value of an f-search. Validation rules are applied on the value depending on the type property of the f-text-input."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:a})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"placeholder",children:"placeholder"}),`
+`,e.jsx(s.p,{children:"Defines the placeholder text for f-text-input"}),`
+`,e.jsx(r,{children:e.jsx(n,{of:p})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-search can have 2 sizes."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:f})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:'height="28px"'}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:'eheight="36px"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"state",children:"state"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:u})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"default"}),e.jsx("td",{}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"primary"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"success"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"danger"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"warning"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"result",children:"result"}),`
+`,e.jsx(s.p,{children:"Provides the search result in the form of an array of strings."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:b})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"object of categories"}),e.jsx("td",{children:'for eg. category1:["option1", "option2"]'}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"array os string values"}),e.jsx("td",{children:'for e.g. ["option1", "option2"]'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"scope",children:"scope"}),`
+`,e.jsx(s.p,{children:"Provides a select dropdown on the left to filter the search query."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:m})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"none"}),e.jsx("td",{children:"no scope"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"array os string values"}),e.jsx("td",{children:'for e.g. ["option1", "option2"]'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"flags",children:"Flags"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(r,{children:e.jsx(n,{of:g})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Name"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"search-button"}),e.jsx("td",{children:e.jsx(s.p,{children:`Note: Provides an icon button to trigger the search query. Note: Search icon on left is not
+shown if search-button is true`})}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"disabled"}),e.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"clear"}),e.jsx("td",{children:e.jsx(s.p,{children:`Displays a close icon-button on the right side of the search that allows the user to clear
+the input value`})}),e.jsx("td",{children:e.jsx("code",{children:"true"})})]})]})}),`
+`,e.jsx(t,{of:"f-search"})]})}function z(d={}){const{wrapper:s}={...c(),...d.components};return s?e.jsx(s,{...d,children:e.jsx(i,{...d})}):i(d)}function y(d,s){throw new Error("Expected component `"+d+"` to be defined: you likely forgot to import, pass, or provide it.")}export{z as default};
---
+++ assets/f-table-6ceuTtr7.js
@@ -0,0 +1,54 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-BjBhjGZp.js";import{M as l,C as r,S as n}from"./index-EhmjAGC5.js";import{F as c,P as h,A as o,V as x,S as j,a,H as b,b as m,c as p,d as f}from"./f-table.stories-D7UFICQQ.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-z53kX22p.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";import"./ref-C9q2dBKz.js";import"./directive-helpers-CWXDk_SO.js";import"./directive-CJw_OlP2.js";import"./donwload-file-VY-Bp7cF.js";function t(d){const s={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...i(),...d.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{of:c}),`
+`,e.jsx(s.h1,{id:"f-table",children:"f-table"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Table component includes rows and cells."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-table--playground",children:"Playground"})}),`
+`,e.jsx(r,{children:e.jsx(n,{of:h})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"anatomy",children:"Anatomy"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n,{of:o}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"variant are representations of a f-table with different row background and borders."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:x})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"stripped"}),e.jsx("td",{children:"Sets aletrnate background on each row"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"outlined"}),e.jsx("td",{children:"Sets border around table"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"underlined"}),e.jsx("td",{children:"Sets border at bottom of each row"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"bordered"}),e.jsx("td",{children:"Sets border around each cell"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"size sets differrent padding on each cell of table"}),`
+`,e.jsx(r,{children:e.jsx(n,{of:j})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:"Sets 16px padding on each cell"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:"Sets 12px padding on each cell"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"selectable",children:"selectable"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"selectable sets checkbox or radio for row selection."}),`
+`,e.jsx(r,{children:e.jsx(n,{of:a})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"none"}),e.jsx("td",{children:"Sets no selection on rows"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"single"}),e.jsx("td",{children:"radio available for selection on rows"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"multiple"}),e.jsx("td",{children:"checkbox available for selection on rows"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"highlight-selected",children:"highlight-selected"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"Sets background for selected rows"}),`
+`,e.jsx(r,{children:e.jsx(n,{of:b})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"highlight-hover",children:"highlight-hover"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"Sets background on row hover"}),`
+`,e.jsx(r,{children:e.jsx(n,{of:m})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"highlight-column-hover",children:"highlight-column-hover"}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:"Sets background on column hover"}),`
+`,e.jsx(r,{children:e.jsx(n,{of:p})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"selected-rows",children:"@selected-rows"}),`
+`,e.jsx("hr",{}),`
+`,e.jsxs(s.p,{children:[e.jsx(s.code,{children:"selected-rows"})," event"]}),`
+`,e.jsx(r,{children:e.jsx(n,{of:f})}),`
+`,e.jsx("br",{})]})}function N(d={}){const{wrapper:s}={...i(),...d.components};return s?e.jsx(s,{...d,children:e.jsx(t,{...d})}):t(d)}export{N as default};
