2cb3604155f166904f60c9575e295f8b32c77a9c
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-06-20 03:39:58 +0000

    deploy: 6c6ff7106d3fdfa282ff7d63eb67df68e3c311a0

---
+++ assets/f-text-C3eUVnYR.js
@@ -0,0 +1,53 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-BjBhjGZp.js";import{M as x,C as t,S as n}from"./index-jGVGL_OQ.js";import{F as c,P as h,V as a,S as j,a as o,A as p,H as m,b as f}from"./f-text.stories-B6PSbwpU.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-D9KWPrS1.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";function i(d){const s={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...l(),...d.components},{ArgsTable:r}=s;return r||u("ArgsTable"),e.jsxs(e.Fragment,{children:[e.jsx(x,{of:c}),`
+`,e.jsx(s.h1,{id:"f-text",children:"f-text"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Text component includes Headings, titles, body texts and links."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-text--playground",children:"Playground"})}),`
+`,e.jsx(t,{children:e.jsx(n,{of:h})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"variants of text component are use cases such as Heading, paragraph, and code."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:a})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"heading"}),e.jsx("td",{children:"used for headings"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"para"}),e.jsx("td",{children:"used for paragraphs"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"code"}),e.jsx("td",{children:"for demonstrating a code example"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx(s.p,{children:"Each variant has various sizes. By default medium is the default size."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:j})}),`
+`,e.jsx("br",{}),`
+`,e.jsxs(s.h3,{id:"sizes-for-variantheading",children:["sizes for ",e.jsx(s.code,{children:'variant="heading"'})]}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"x-large"}),e.jsx("td",{children:"Extra Large - 24px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"large"}),e.jsx("td",{children:"Large - 20px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:"Medium - 16px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:"Small - 14px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"x-small"}),e.jsx("td",{children:"Extra Small - 12px"}),e.jsx("td",{})]})]})}),`
+`,e.jsxs(s.h3,{id:"sizes-for-variantpara",children:["sizes for ",e.jsx(s.code,{children:'variant="para"'})]}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"large"}),e.jsx("td",{children:"Large - 16px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:"Medium - 14px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:"Small - 12px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"x-small"}),e.jsx("td",{children:"Extra Small - 10px"}),e.jsx("td",{})]})]})}),`
+`,e.jsxs(s.h3,{id:"sizes-for-variantcode",children:["sizes for ",e.jsx(s.code,{children:'variant="code"'})]}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"large"}),e.jsx("td",{children:"Large - 16px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:"Medium - 14px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:"Small - 12px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"x-small"}),e.jsx("td",{children:"Extra Small - 10px"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"state",children:"state"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:`States on texts are used to communicate purpose and it’s connotation. For example, a red color
+connotes danger, whereas a green color connotes success and so on.`}),`
+`,e.jsx(t,{children:e.jsx(n,{of:o})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"align",children:"align"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Sets the alignment of the text. Can take 3 values: left, center, and right."}),`
+`,e.jsx(t,{children:e.jsx(n,{of:p})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"left"}),e.jsx("td",{children:"text aligned towards left side"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"center"}),e.jsx("td",{children:"text aligned at the center"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"right"}),e.jsx("td",{children:"text aligned towards right side"}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"highlight",children:"highlight"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Highlight's sub-text inside f-text"}),`
+`,e.jsx(t,{children:e.jsx(n,{of:m})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"flags",children:"Flags"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:`Flag properties change visually and/or programmatically depending on user interaction or a
+predetermined state.`}),`
+`,e.jsx(t,{children:e.jsx(n,{of:f})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Name"}),e.jsx("td",{children:"Description"}),e.jsx("td",{children:"Default"})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"inline"}),e.jsx("td",{children:"width of text would be fit-content"}),e.jsx("td",{children:"false"})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"loading"}),e.jsx("td",{children:"skeleton will replace the text"}),e.jsx("td",{children:"false"})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"disabled"}),e.jsx("td",{children:"Opacity is set to 50% in disabled state."}),e.jsx("td",{children:"false "})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"ellipsis"}),e.jsx("td",{children:e.jsx(s.p,{children:`By default no. of rows is to 1. Ellipsis will crop the text after one line w.r.t the outer
+div`})}),e.jsx("td",{children:"false"})]})]})}),`
+`,e.jsx(r,{of:"f-text"})]})}function N(d={}){const{wrapper:s}={...l(),...d.components};return s?e.jsx(s,{...d,children:e.jsx(i,{...d})}):i(d)}function u(d,s){throw new Error("Expected component `"+d+"` to be defined: you likely forgot to import, pass, or provide it.")}export{N as default};
