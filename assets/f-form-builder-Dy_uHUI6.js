5e19c13e72e171188706c3e96e99f064804402da
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-06-20 05:48:50 +0000

    deploy: f0bc706148285294a0707dc08558f217f55f805b

---
+++ assets/f-form-builder-Dy_uHUI6.js
@@ -0,0 +1,11 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-BjBhjGZp.js";import{M as e,C as s,S as m}from"./index-BPu8aY2B.js";import"./lit-html-BulfDIla.js";import{F as a,P as d}from"./f-form-builder.stories-CrB2kfF-.js";import p from"./properties-D8uPT0RP.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CXRRsDdn.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-element-3mBpJhh8.js";import"./ref-C9q2dBKz.js";import"./directive-helpers-CWXDk_SO.js";import"./directive-CJw_OlP2.js";import"./chunk-2WNKQWTL-cTwTzLwY.js";function t(n){const o={a:"a",h1:"h1",h2:"h2",h3:"h3",...i(),...n.components};return r.jsxs(r.Fragment,{children:[r.jsx(e,{of:a}),`
+`,r.jsx(o.h1,{id:"f-form-builder",children:"f-form-builder"}),`
+`,r.jsx("br",{}),`
+`,r.jsx("br",{}),`
+`,r.jsx(o.h2,{id:"overview",children:"Overview"}),`
+`,r.jsx("f-divider",{}),`
+`,r.jsx("br",{}),`
+`,r.jsx(o.h3,{id:"playground",children:r.jsx(o.a,{href:"/story/components-f-form-builder--playground",children:"Playground"})}),`
+`,r.jsx(s,{children:r.jsx(m,{of:d})}),`
+`,r.jsx("br",{}),`
+`,r.jsx(p,{})]})}function P(n={}){const{wrapper:o}={...i(),...n.components};return o?r.jsx(o,{...n,children:r.jsx(t,{...n})}):t(n)}export{P as default};
