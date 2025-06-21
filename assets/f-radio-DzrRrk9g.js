aa1028640ea9b1f0973c668862cc59da1977a0c3
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-06-21 07:48:21 +0000

    deploy: c1c4af554a10417a69bbd7cfa69e31020b4bd518

---
+++ assets/f-radio-DzrRrk9g.js
@@ -0,0 +1,38 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as o}from"./index-BjBhjGZp.js";import{M as l,C as r,S as d}from"./index-BjVUF9E_.js";import{F as x,P as c,A as j,V as a,S as h,a as m,b as p}from"./f-radio.stories-QkcgkPoi.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BFD_Max2.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import"./directive-CJw_OlP2.js";import"./chunk-2WNKQWTL-cTwTzLwY.js";function i(n){const e={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...o(),...n.components},{ArgsTable:t}=e;return t||f("ArgsTable"),s.jsxs(s.Fragment,{children:[s.jsx(l,{of:x}),`
+`,s.jsx(e.h1,{id:"f-radio",children:"f-radio"}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"Radio allow users to choose a single option among a set of mutually exclusive options."}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"playground",children:s.jsx(e.a,{href:"/story/components-f-radio--playground",children:"Playground"})}),`
+`,s.jsx(r,{children:s.jsx(d,{of:c})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h3,{id:"anatomy",children:"Anatomy"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(d,{of:j}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"value",children:"value"}),`
+`,s.jsx(e.p,{children:"Value of a switch defines if it is on or off."}),`
+`,s.jsx(r,{children:s.jsx(d,{of:a})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"selected"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"unselected"}),s.jsx("td",{}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"size",children:"size"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"f-switch can have 2 sizes."}),`
+`,s.jsx(r,{children:s.jsx(d,{of:h})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"small"}),s.jsx("td",{children:'size="12px"'}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"medium"}),s.jsx("td",{children:'size="16px"'}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"state",children:"state"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx(e.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,s.jsx(r,{children:s.jsx(d,{of:m})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Value"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"default"}),s.jsx("td",{}),s.jsx("td",{children:s.jsx("code",{children:"default"})})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"primary"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"success"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"danger"}),s.jsx("td",{}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"warning"}),s.jsx("td",{}),s.jsx("td",{})]})]})}),`
+`,s.jsx("br",{}),`
+`,s.jsx(e.h2,{id:"flags",children:"Flags"}),`
+`,s.jsx("f-divider",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r,{children:s.jsx(d,{of:p})}),`
+`,s.jsx("table",{className:"custom-table",children:s.jsxs("tbody",{children:[s.jsxs("tr",{children:[s.jsx("td",{children:"Name"}),s.jsx("td",{children:"Description"}),s.jsx("td",{})]}),s.jsxs("tr",{children:[s.jsx("td",{children:"disabled"}),s.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),s.jsx("td",{children:s.jsx("code",{children:"false"})})]})]})}),`
+`,s.jsx(t,{of:"f-switch"})]})}function N(n={}){const{wrapper:e}={...o(),...n.components};return e?s.jsx(e,{...n,children:s.jsx(i,{...n})}):i(n)}function f(n,e){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{N as default};
