7127d1ac84b120bde62170961199a17424d91dfd
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-07-28 12:18:09 +0000

    deploy: 42d7074906ff1052f3b7b135e158ae5d57cd480b

---
+++ assets/f-form-DIF3-QiA.js
@@ -0,0 +1,22 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as d}from"./index-BjBhjGZp.js";import{M as x,C as o,S as t}from"./index-h3LJ358n.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import{F as l,P as j,A as a,G as m}from"./f-form.stories-CQyI9mHY.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-yCPQfhiy.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./directive-CJw_OlP2.js";import"./chunk-2WNKQWTL-cTwTzLwY.js";function i(s){const r={a:"a",h1:"h1",h2:"h2",h3:"h3",p:"p",...d(),...s.components},{ArgsTable:e}=r;return e||c("ArgsTable"),n.jsxs(n.Fragment,{children:[n.jsx(x,{of:l}),`
+`,n.jsx(r.h1,{id:"f-form",children:"f-form"}),`
+`,n.jsx("br",{}),`
+`,n.jsx("br",{}),`
+`,n.jsx(r.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(r.p,{children:"f-from component helps to systematically layout and build forms in Flow 2"}),`
+`,n.jsx("br",{}),`
+`,n.jsx(r.h3,{id:"playground",children:n.jsx(r.a,{href:"/story/components-f-form--playground",children:"Playground"})}),`
+`,n.jsx(o,{children:n.jsx(t,{of:j})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(r.h3,{id:"anatomy",children:"Anatomy"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(t,{of:a}),`
+`,n.jsx("br",{}),`
+`,n.jsx(r.h2,{id:"gap",children:"gap"}),`
+`,n.jsx("f-divider",{}),`
+`,n.jsx(r.p,{children:"Controls gap between all input elements within the form"}),`
+`,n.jsx(o,{children:n.jsx(t,{of:m})}),`
+`,n.jsx("table",{className:"custom-table",children:n.jsxs("tbody",{children:[n.jsxs("tr",{children:[n.jsx("td",{children:"Value"}),n.jsx("td",{children:"Description"}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"x-small"}),n.jsx("td",{}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"small"}),n.jsx("td",{}),n.jsx("td",{})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"medium"}),n.jsx("td",{}),n.jsx("td",{children:n.jsx("code",{children:"default"})})]}),n.jsxs("tr",{children:[n.jsx("td",{children:"large"}),n.jsx("td",{}),n.jsx("td",{})]})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e,{of:"f-form"})]})}function E(s={}){const{wrapper:r}={...d(),...s.components};return r?n.jsx(r,{...s,children:n.jsx(i,{...s})}):i(s)}function c(s,r){throw new Error("Expected component `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{E as default};
