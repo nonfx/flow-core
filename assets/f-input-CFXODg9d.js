2459d173b7831c9df5e5056accb4c89981e79af2
PercyShadrach <PercyShadrach@users.noreply.github.com>
2025-07-29 10:43:08 +0000

    deploy: bb3a93e2b4c0634a40272cb8bc4650ba40721515

---
+++ assets/f-input-CFXODg9d.js
@@ -0,0 +1,83 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-BjBhjGZp.js";import{M as x,C as d,S as n}from"./index-CfzIYRFT.js";import{F as c,P as h,A as j,V as a,C as o,T as f,a as p,b as u,S as m,c as b,I as g,d as y,e as v,f as w,M as D,g as N}from"./f-input.stories-OHszcjU6.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BwnwuzmO.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";import"./i-finput-anatomy-DZtGO_DG.js";import"./unsafe-svg-Bgti8cH4.js";import"./directive-CJw_OlP2.js";function i(r){const s={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...l(),...r.components},{ArgsTable:t}=s;return t||M("ArgsTable"),e.jsxs(e.Fragment,{children:[e.jsx(x,{of:c}),`
+`,e.jsx(s.h1,{id:"f-input",children:"f-input"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-input is an input element."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"playground",children:e.jsx(s.a,{href:"/story/components-f-input--playground",children:"Playground"})}),`
+`,e.jsx(d,{children:e.jsx(n,{of:h})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"anatomy",children:"Anatomy"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(n,{of:j}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"variant",children:"variant"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Variants are various visual representations of an input field."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:a})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"curved"}),e.jsx("td",{children:"border radius of 4px"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"round"}),e.jsx("td",{children:"border radius of 22px"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"block"}),e.jsx("td",{children:'works well with cateogry="transparent"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"category",children:"category"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"Category decides the background of f-input"}),`
+`,e.jsx(d,{children:e.jsx(n,{of:o})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"fill"}),e.jsx("td",{children:"background is filled"}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"outline"}),e.jsx("td",{children:"empty background with border"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"transparent"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"type",children:"type"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsxs(s.p,{children:["The type attribute specifies the type of ",e.jsx(s.code,{children:"<f-input>"})," element to display."]}),`
+`,e.jsx(d,{children:e.jsx(n,{of:f})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"text"}),e.jsx("td",{children:"Takes any text as input. "}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"number"}),e.jsx("td",{children:"Takes any number as input."}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"email"}),e.jsx("td",{children:"Defines a field for an e-mail address."}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"password"}),e.jsx("td",{children:"Defines a field for a password. Masks the characters"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"tel"}),e.jsx("td",{children:"Defines a field for a telephone number."}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"url"}),e.jsx("td",{children:"Defines a field for URL. "}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"value",children:"value"}),`
+`,e.jsx(s.p,{children:"Defines the value of an f-input. Validation rules are applied on the value depending on the type property of the f-text-input."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:p})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"placeholder",children:"placeholder"}),`
+`,e.jsx(s.p,{children:"Defines the placeholder text for f-text-input"}),`
+`,e.jsx(d,{children:e.jsx(n,{of:u})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"size",children:"size"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"f-input can have 2 sizes."}),`
+`,e.jsx("p",{className:"note-section",children:"Note: By default size is inherited by the parent f-form."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:m})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"small"}),e.jsx("td",{children:'height="28px"'}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"medium"}),e.jsx("td",{children:'eheight="36px"'}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"state",children:"state"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(s.p,{children:"States are used to communicate purpose and connotations. For example, a red color connotes danger, whereas a green color connotes success and so on."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:b})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Value"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"default"}),e.jsx("td",{}),e.jsx("td",{children:e.jsx("code",{children:"default"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"primary"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"success"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"danger"}),e.jsx("td",{}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"warning"}),e.jsx("td",{}),e.jsx("td",{})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"icon-left",children:"icon-left"}),`
+`,e.jsx(s.p,{children:"Icon-left enables an icon on the left of the input value."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(s.p,{children:"Note: If f-input has a prefix also, then icon-left is placed on the left of the prefix."})}),`
+`,e.jsx(d,{children:e.jsx(n,{of:g})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"icon-right",children:"icon-right"}),`
+`,e.jsx(s.p,{children:"Icon-right enables an icon on the right of the input value."}),`
+`,e.jsx("p",{className:"note-section",children:e.jsx(s.p,{children:"Note: If f-input has a suffix also, then icon-right is placed on the right of the suffix."})}),`
+`,e.jsx(d,{children:e.jsx(n,{of:y})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"prefix",children:"prefix"}),`
+`,e.jsx(s.p,{children:"Prefix property enables a string before the input value."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:v})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"suffix",children:"suffix"}),`
+`,e.jsx(s.p,{children:"Suffix property enables a string on the right side of the input box."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:w})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"max-length",children:"max-length"}),`
+`,e.jsx(s.p,{children:"This shows the character count while typing and auto limits after reaching the max length."}),`
+`,e.jsx(d,{children:e.jsx(n,{of:D})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"flags",children:"Flags"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(d,{children:e.jsx(n,{of:N})}),`
+`,e.jsx("table",{className:"custom-table",children:e.jsxs("tbody",{children:[e.jsxs("tr",{children:[e.jsx("td",{children:"Name"}),e.jsx("td",{children:"Description"}),e.jsx("td",{})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"loading"}),e.jsx("td",{children:"Loader appears on right ride of input box."}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"disabled"}),e.jsx("td",{children:"Opacity is set to 50% in disabled state in disabled mode."}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"clear"}),e.jsx("td",{children:e.jsx(s.p,{children:`Displays a close icon-button on the right side of the input that allows the user to clear
+the input value`})}),e.jsx("td",{children:e.jsx("code",{children:"true"})})]}),e.jsxs("tr",{children:[e.jsx("td",{children:"read-only"}),e.jsx("td",{children:"Only Text could be read, can't be edited"}),e.jsx("td",{children:e.jsx("code",{children:"false"})})]})]})}),`
+`,e.jsx(t,{of:"f-input"})]})}function X(r={}){const{wrapper:s}={...l(),...r.components};return s?e.jsx(s,{...r,children:e.jsx(i,{...r})}):i(r)}function M(r,s){throw new Error("Expected component `"+r+"` to be defined: you likely forgot to import, pass, or provide it.")}export{X as default};
---
+++ assets/node-details-template-BDvlJy5Z.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-BjBhjGZp.js";import{M as s,S as t,C as a}from"./index-CfzIYRFT.js";import{N as d,A as p,a as m}from"./node-details-template.stories-BYkFNsQJ.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BwnwuzmO.js";import"./index-ipr2_oRq.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";import"./lit-html-BulfDIla.js";import"./unsafe-svg-Bgti8cH4.js";import"./directive-CJw_OlP2.js";import"./chunk-2WNKQWTL-cTwTzLwY.js";function i(n){const o={h1:"h1",h2:"h2",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:d}),`
+`,e.jsx(o.h1,{id:"node-details-popover",children:"Node-details Popover"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h1,{id:"overview",children:"Overview"}),`
+`,e.jsx(o.p,{children:`In flow-lineage, node-details popover template provides meta information of a node.
+A user can open node-details popover by a double-click on a node.`}),`
+`,e.jsx(o.p,{children:"This template uses f-popover."}),`
+`,e.jsx(o.h2,{id:"anatomy",children:"Anatomy"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h1,{id:"available-templates",children:"Available Templates"}),`
+`,e.jsx(o.h2,{id:"node-details",children:"node-details"}),`
+`,e.jsx("f-divider",{}),`
+`,e.jsx(a,{children:e.jsx(t,{of:m})})]})}function w(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(i,{...n})}):i(n)}export{w as default};
